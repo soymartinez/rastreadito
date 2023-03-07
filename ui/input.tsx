ec6340b40b35615f25f91a21122cd3441b5f1bb9
omgismartinez <martnzomg@gmail.com>
2023-03-06 23:08:42 -0600

    🌌 refactor (input): icons + design

--- ui/input.tsx
+++ ui/input.tsx
@@ -2,20 +2,60 @@ import * as React from 'react'
 
 import clsx from 'clsx'
 
+import { Mail } from 'lucide-react'
+import { Label } from './label'
+
+interface InputPropsExtended {
+    icon?: 'email',
+    labelText?: string,
+}
+
+export interface IconProps
+    extends React.HTMLAttributes<SVGSVGElement>, InputPropsExtended { }
+
 export interface InputProps
-    extends React.InputHTMLAttributes<HTMLInputElement> { }
+    extends React.InputHTMLAttributes<HTMLInputElement>, InputPropsExtended { }
+
+const Icons = React.forwardRef<HTMLDivElement, IconProps>(
+    ({ icon, ...props }, ref) => {
+        return (
+            <>
+                {icon === 'email' && <Mail {...props} />}
+            </>
+        )
+    }
+)
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-    ({ className, ...props }, ref) => {
+    ({ className, labelText, icon, ...props }, ref) => {
         return (
-            <input
+            <div
                 className={clsx(
-                    'flex h-10 w-full rounded-md border border-slate-300 bg-transparent py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900',
+                    'flex items-center gap-3 w-full rounded-2xl border border-_primary bg-transparent py-3 px-5 text-base font-medium placeholder:text-_grayTextDisabled focus:outline-none focus:ring-2 focus:ring-_primary focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-_dark dark:text-_dark dark:focus:ring-_primary dark:focus:ring-offset-_primary',
                     className
                 )}
                 ref={ref}
                 {...props}
-            />
+            >
+                {icon &&
+                    <>
+                        <Icons icon={icon} className={'text-_dark mx-2'} />
+                        <div className='bg-_primary w-px h-10 rounded-full' />
+                    </>
+                }
+                <div className='flex flex-col'>
+                    <Label htmlFor='input' className='text-_primary text-xs'>{labelText}</Label>
+                    <input
+                        id='input'
+                        className={clsx(
+                            'flex w-full bg-transparent text-base font-medium placeholder:text-_grayTextLight focus:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+                            className
+                        )}
+                        ref={ref}
+                        {...props}
+                    />
+                </div>
+            </div>
         )
     }
 )
