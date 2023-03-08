e1d2a6cbbee67ce563f97333e82daaf086c04af6
omgismartinez <martnzomg@gmail.com>
2023-03-07 21:01:34 -0600

    🌌 refactor (input): useId + width full

--- ui/input.tsx
+++ ui/input.tsx
@@ -28,10 +28,11 @@ const Icons = React.forwardRef<HTMLDivElement, IconProps>(
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
     ({ className, labelText, icon, ...props }, ref) => {
+        const id = React.useId()
         return (
             <div
                 className={clsx(
-                    'flex items-center gap-3 w-full rounded-2xl border border-_primary bg-transparent py-3 px-5 text-base font-medium placeholder:text-_grayTextDisabled focus:outline-none focus:ring-2 focus:ring-_primary focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-_dark dark:text-_dark dark:focus:ring-_primary dark:focus:ring-offset-_primary',
+                    'flex h-16 items-center gap-3 w-full rounded-2xl border border-_primary bg-transparent py-3 px-5 text-base font-medium placeholder:text-_grayTextDisabled focus:outline-none focus:ring-2 focus:ring-_primary focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-_dark dark:text-_dark dark:focus:ring-_primary dark:focus:ring-offset-_primary',
                     className
                 )}
                 ref={ref}
@@ -43,10 +44,10 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
                         <div className='bg-_primary w-px h-10 rounded-full' />
                     </>
                 }
-                <div className='flex flex-col'>
-                    <Label htmlFor='input' className='text-_primary text-xs'>{labelText}</Label>
+                <div className='flex flex-col w-full'>
+                    <Label htmlFor={`input-${id}`} className='text-_primary text-xs'>{labelText}</Label>
                     <input
-                        id='input'
+                        id={`input-${id}`}
                         className={clsx(
                             'flex w-full bg-transparent text-base font-medium placeholder:text-_grayTextLight focus:outline-none disabled:cursor-not-allowed disabled:opacity-50',
                             className
