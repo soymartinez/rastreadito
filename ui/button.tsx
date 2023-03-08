1419e7ac90214682dfb4876015a7519ddbaaf7b4
omgismartinez <martnzomg@gmail.com>
2023-03-07 20:57:19 -0600

    🌌 refactor (button): `default` style

--- ui/button.tsx
+++ ui/button.tsx
@@ -4,12 +4,12 @@ import { VariantProps, cva } from 'class-variance-authority'
 import clsx from 'clsx'
 
 const buttonVariants = cva(
-    'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800',
+    'active:scale-95 inline-flex items-center justify-center rounded-2xl text-xl font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-_primary focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none data-[state=open]:bg-_primary',
     {
         variants: {
             variant: {
                 default:
-                    'bg-slate-900 text-white hover:bg-slate-700 dark:bg-slate-50 dark:text-slate-900',
+                    'bg-_primary text-_dark hover:bg-_primary/80',
                 destructive:
                     'bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600',
                 outline:
@@ -21,7 +21,7 @@ const buttonVariants = cva(
                 link: 'bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent',
             },
             size: {
-                default: 'h-10 py-2 px-4',
+                default: 'h-16 py-2 px-4',
                 sm: 'h-9 px-2 rounded-md',
                 lg: 'h-11 px-8 rounded-md',
             },
