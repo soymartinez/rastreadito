105152f2d545c27c56523ecb06493e494516fdaa
omgismartinez <martnzomg@gmail.com>
2023-03-09 23:44:34 -0600

    🚀 feat (ui): textarea

---
+++ ui/textarea.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx'
+import * as React from 'react'
+import { Label } from './label'
+
+interface TextareaPropsExtended {
+    labelText?: string,
+}
+
+export interface TextareaProps
+    extends React.TextareaHTMLAttributes<HTMLTextAreaElement>, TextareaPropsExtended { }
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+    ({ labelText, className, ...props }, ref) => {
+        const id = React.useId()
+        return (
+            <div
+                className={clsx(
+                    'flex items-center gap-3 w-full rounded-2xl border border-_primary bg-transparent py-3 px-5 text-base font-medium placeholder:text-_grayTextDisabled focus:outline-none focus:ring-2 focus:ring-_primary focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-_dark dark:text-_dark dark:focus:ring-_primary dark:focus:ring-offset-_primary',
+                    className
+                )}
+            >
+                <div className='flex flex-col w-full'>
+                    <Label htmlFor={`textarea-${id}`} className='text-_primary text-xs'>{labelText}</Label>
+                    <textarea
+                        id={`textarea-${id}`}
+                        className={clsx(
+                            'flex h-20 w-full bg-transparent text-base font-medium placeholder:text-_grayTextLight focus:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+                            className
+                        )}
+                        ref={ref}
+                        {...props}
+                    />
+                </div>
+            </div>
+        )
+    }
+)
+Textarea.displayName = 'Textarea'
+
+export { Textarea }
