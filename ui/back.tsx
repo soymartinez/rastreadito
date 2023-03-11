838e6e1b23733afdbc1e34077aa5eefb43f85509
omgismartinez <martnzomg@gmail.com>
2023-03-11 16:00:12 -0600

    🎯 fix (back): attempted import error

--- ui/back.tsx
+++ ui/back.tsx
@@ -3,13 +3,13 @@
 import React from 'react'
 
 import { useRouter } from 'next/navigation'
-import { ChevronLeftIcon, LucideProps } from 'lucide-react'
+import { ChevronLeft, LucideProps } from 'lucide-react'
 import clsx from 'clsx'
 
 const Back = React.forwardRef<SVGSVGElement, LucideProps>(
     ({ className, ...props }, ref) => {
         const router = useRouter()
-        return <ChevronLeftIcon
+        return <ChevronLeft
             ref={ref}
             className={clsx('text-_dark mx-3 cursor-pointer', className)}
             size={32}
