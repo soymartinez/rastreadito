905dff0935dd105c99e7c171538dc3124f28804f
omgismartinez <martnzomg@gmail.com>
2023-03-11 17:03:59 -0600

    🌌 refactor (metadata): full design

--- app/metadata/page.tsx
+++ app/metadata/page.tsx
@@ -1,7 +1,7 @@
 import { Back } from '@/ui/back'
 import { Input } from '@/ui/input'
 import { Textarea } from '@/ui/textarea'
-import { Info } from 'lucide-react'
+import { Info, Save } from 'lucide-react'
 
 import {
     Select,
@@ -10,7 +10,16 @@ import {
     SelectTrigger,
     SelectValue,
 } from '@/ui/select'
+import { Button } from '@/ui/button'
 
+const productTypes = {
+    flower: 'Flor de cannabis',
+    tincture: 'Goteros de cannabis',
+    topical: 'Ungüento de cannabis',
+    edible: 'Edibles de cannabis',
+    cartridge: 'Carts de cannabis',
+    oil: 'Aceite de cannabis',
+}
 
 export default function Metadata() {
     return (
@@ -19,27 +28,46 @@ export default function Metadata() {
                 <Back className='absolute left-0' />
                 <h1 className='font-bold text-xl'>Metadata</h1>
             </div>
-            <section className='flex flex-col gap-4'>
-                <div className='flex justify-between'>
-                    <h3 className='font-semibold'>Información</h3>
-                    <Info />
+            <form className='flex flex-col gap-7'>
+                <div className='flex flex-col gap-4'>
+                    <div className='flex justify-between'>
+                        <h3 className='font-semibold'>Información</h3>
+                        <Info />
+                    </div>
+                    <div className='flex flex-col gap-3'>
+                        <Input labelText='Nombre' placeholder='Purple Kush' required />
+                        <Textarea labelText='Descripción' placeholder='Purple Kush' required />
+                        <Select required>
+                            <SelectTrigger labelText='Tipo de producto'>
+                                <SelectValue placeholder='Selecciona un tipo de producto' />
+                            </SelectTrigger>
+                            <SelectContent>
+                                {Object.entries(productTypes).map(([key, value]) => (
+                                    <SelectItem className='first:rounded-t-xl last:rounded-b-xl placeholder:text-_primary' key={key} value={key}>
+                                        {value}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    </div>
                 </div>
-                <div className='flex flex-col gap-3'>
-                    <Input labelText='Nombre' placeholder='Purple Kush' required />
-                    <Textarea labelText='Descripción' placeholder='Purple Kush' required />
-                    <Select>
-                        <SelectTrigger>
-                            <SelectValue placeholder='Theme' />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectItem value='light'>Light</SelectItem>
-                            <SelectItem value='dark'>Dark</SelectItem>
-                            <SelectItem value='system'>System</SelectItem>
-                        </SelectContent>
-                    </Select>
-
+                <div className='flex flex-col gap-4'>
+                    <div className='flex justify-between'>
+                        <h3 className='font-semibold'>Características</h3>
+                        <Info />
+                    </div>
+                    <div className='flex flex-col gap-3'>
+                        <Input labelText='Cepa' placeholder='Purple Kush' required />
+                        <Input labelText='THC' placeholder='0.0' required variant='porcentage' />
+                        <Input labelText='CBD' placeholder='0.0' required variant='porcentage' />
+                        <Input labelText='Aroma' placeholder='Tierra, pino, dulce' required />
+                        <Input labelText='Efectos' placeholder='Relajante, calmante, eufórico' required />
+                    </div>
                 </div>
-            </section>
+                <Button className='w-16 fixed right-4 bottom-8'>
+                    <Save />
+                </Button>
+            </form>
         </div>
     )
 }
