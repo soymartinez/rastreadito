8d1286f9c805b4aab70926f21119a17219a08deb
omgismartinez <martnzomg@gmail.com>
2023-03-21 22:36:14 -0600

    🌌 refactor (home): header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import { Bell } from 'lucide-react'
+
 export default function Home() {
   return (
-    <main>
-      <h1>QR</h1>
+    <main className='px-4 min-h-screen relative'>
+      <div className='flex justify-between items-center py-8 relative'>
+        <h1 className='font-black text-2xl uppercase italic'>weedtrace</h1>
+        <div className='flex gap-2'>
+          <div className='w-12 h-12 border-2 rounded-full flex items-center justify-center'>
+            <Bell />
+          </div>
+          <div className='w-12 h-12 border-2 bg-_primary rounded-full' />
+        </div>
+      </div>
+      <h1 className='text-5xl font-bold leading-loose'>BeeHealthy</h1>
     </main>
   )
 }
