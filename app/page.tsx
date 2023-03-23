b6e03ebb602c81729f5e1b8f543c4c86c4a192e8
omgismartinez <martnzomg@gmail.com>
2023-03-22 22:52:12 -0600

    🌌 refactor (home): general section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Bell } from 'lucide-react'
+import { Bell, LayoutGrid, Package, PackageOpen, QrCode } from 'lucide-react'
 import Link from 'next/link'
 
 export default function Home() {
@@ -28,6 +28,46 @@ export default function Home() {
           </div>
         </Link>
       </div>
+      <section>
+        <div className='flex justify-between items-center pt-6 pb-3'>
+          <h1 className='font-semibold text-xl'>General</h1>
+          <button className='p-2 border-2 border-_gray rounded-full'>
+            <LayoutGrid />
+          </button>
+        </div>
+        <div className='grid gap-3'>
+          <div className='grid grid-flow-col-dense auto-cols-auto mx-auto gap-4 justify-start items-center bg-_dark p-4 rounded-2xl'>
+            <QrCode size={56} className='text-_primary' />
+            <div>
+              <h1 className='text-xl font-semibold text-_white'>Activo</h1>
+              <p className='text-xs font-medium text-_grayText'>
+                El código aún no ha sido escaneado y utilizado para su propósito previsto.
+              </p>
+            </div>
+            <h1 className='text-5xl font-semibold text-_white'>86</h1>
+          </div>
+          <div className='grid grid-flow-col-dense auto-cols-auto mx-auto gap-4 justify-start items-center bg-_dark p-4 rounded-2xl'>
+            <Package size={56} className='text-_primary' />
+            <div>
+              <h1 className='text-xl font-semibold text-_white'>Uso</h1>
+              <p className='text-xs font-medium text-_grayText'>
+                El código ha sido utilizado y ya no es válido. Evitar el fraude o la duplicación de códigos.
+              </p>
+            </div>
+            <h1 className='text-5xl font-semibold text-_white'>62</h1>
+          </div>
+          <div className='grid grid-flow-col-dense auto-cols-auto mx-auto gap-4 justify-start items-center bg-_dark p-4 rounded-2xl'>
+            <PackageOpen size={56} className='text-_primary' />
+            <div>
+              <h1 className='text-xl font-semibold text-_white'>Destruido</h1>
+              <p className='text-xs font-medium text-_grayText'>
+                Un ticket destruido no puede ser utilizado y su estado indica que ha sido anulado.
+              </p>
+            </div>
+            <h1 className='text-5xl font-semibold text-_white'>24</h1>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
