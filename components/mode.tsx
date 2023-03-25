2063c2c013cfb47f2f4219d146b2e70ec4099546
omgismartinez <martnzomg@gmail.com>
2023-03-24 22:17:36 -0600

    🌌 refactor (mode): dark mode support

--- components/mode.tsx
+++ components/mode.tsx
@@ -15,21 +15,21 @@ export default function Mode() {
     const { setTheme } = useTheme()
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger asChild className='text-_darkText focus:outline-none'>
+            <DropdownMenuTrigger asChild className='focus:outline-none'>
                 <Button variant='ghost' size='sm'>
-                    <Sun className='rotate-0 scale-100 transition-all hover:text-_darkText dark:-rotate-90 dark:scale-0 dark:text-_white dark:hover:text-_white' />
-                    <Moon className='absolute rotate-90 scale-0 transition-all hover:text-_darkText dark:rotate-0 dark:scale-100 dark:text-_white dark:hover:text-_white' />
+                    <Sun className='rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 dark:text-_primary dark:hover:bg-_darkText' />
+                    <Moon className='absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:text-_primary' />
                     <span className='sr-only'>Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-                <DropdownMenuItem onClick={() => setTheme('light')} className='flex gap-3'>
+                <DropdownMenuItem onClick={() => setTheme('light')} className='flex gap-3 text-_darkText dark:text-_white'>
                     <Sun size={18} /> Claro
                 </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme('dark')} className='flex gap-3'>
+                <DropdownMenuItem onClick={() => setTheme('dark')} className='flex gap-3 text-_darkText dark:text-_white'>
                     <Moon size={18} /> Oscuro
                 </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme('system')} className='flex gap-3'>
+                <DropdownMenuItem onClick={() => setTheme('system')} className='flex gap-3 text-_darkText dark:text-_white'>
                     <Laptop size={18} /> Sistema
                 </DropdownMenuItem>
             </DropdownMenuContent>
