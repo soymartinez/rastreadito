b081265f8a10bea5ac65a4e24dbdcd1c3b8913aa
omgismartinez <martnzomg@gmail.com>
2023-03-24 20:03:25 -0600

    🌌 refactor (provider): theme provider latest config

--- components/provider.tsx
+++ components/provider.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { ThemeProviderProps } from 'next-themes/dist/types'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-    return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+    return <NextThemesProvider>{children}</NextThemesProvider>
 }
