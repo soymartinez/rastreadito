e41372004a1bdd0bd9fead6aa7528646f74f55fa
omgismartinez <martnzomg@gmail.com>
2023-03-24 22:16:57 -0600

    🌌 refactor (button): background gray in variants

--- ui/button.tsx
+++ ui/button.tsx
@@ -13,12 +13,12 @@ const buttonVariants = cva(
                 destructive:
                     'bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600',
                 outline:
-                    'bg-transparent border border-slate-200 hover:bg-slate-100 dark:border-slate-700 dark:text-slate-100',
+                    'bg-transparent border border-slate-200 hover:bg-_gray dark:border-slate-700 dark:text-_gray',
                 subtle:
-                    'bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100',
+                    'bg-_gray text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:text-_gray',
                 ghost:
-                    'bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-100 dark:hover:text-slate-100 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent',
-                link: 'bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent',
+                    'bg-transparent hover:bg-_gray dark:hover:bg-_darkText dark:text-_primary data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent',
+                link: 'bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-_gray hover:bg-transparent dark:hover:bg-transparent',
             },
             size: {
                 default: 'h-16 py-2 px-4',
