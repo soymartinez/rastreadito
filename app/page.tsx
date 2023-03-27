8d3d2bfbed26ecbc9a695513fc87ac45936a7f5e
omgismartinez <martnzomg@gmail.com>
2023-03-27 12:09:55 -0600

    🌌 refactor (home): name changed to `rastreadito`

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <main className='px-4 min-h-screen relative'>
       <div className='flex justify-between items-center py-6 relative'>
-        <h1 className='font-black text-2xl uppercase italic'>weedtrace</h1>
+        <h1 className='font-black text-2xl uppercase italic'>rastreadito</h1>
         <div className='flex gap-2'>
           <Link href={'/scan'} className='rounded-full'>
             <div className='w-11 h-11 border-2 border-_gray dark:border-_darkText rounded-full flex items-center justify-center'>
