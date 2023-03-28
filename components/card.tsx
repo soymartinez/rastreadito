b1324a00350e1c49f283a6945a8c2e51a3803ef3
omgismartinez <martnzomg@gmail.com>
2023-03-27 21:56:23 -0600

    🌌 refactor (card): export cards + PricingCard

--- components/card.tsx
+++ components/card.tsx
@@ -1,15 +1,15 @@
 import clsx from 'clsx'
 import { Package, PackageOpen, QrCode } from 'lucide-react'
+import Balancer from 'react-wrap-balancer'
+import { Button } from '@/ui/button'
 
-interface CardProps {
-    title: string
-    description: string
-    number: number,
-    icon: 'active' | 'use' | 'destroy',
-}
-
-export default function Card({ props, layoutGrid }: {
-    props: CardProps,
+function GeneralCard({ props, layoutGrid }: {
+    props: {
+        title: string
+        description: string
+        number: number,
+        icon: 'active' | 'use' | 'destroy',
+    },
     layoutGrid: 'layout-list' | 'layout-grid',
 }) {
     return (
@@ -33,4 +33,45 @@ export default function Card({ props, layoutGrid }: {
             })}>{props.number}</h1>
         </div>
     )
+}
+
+function PricingCard({
+    name,
+    price,
+    duration,
+    description,
+    button,
+    popular,
+}: {
+    name: string,
+    price: string,
+    duration?: string,
+    description: string,
+    button: string,
+    popular?: boolean,
+}) {
+    return (
+        <div className='flex flex-col justify-between relative items-center gap-4 p-12 border-2 border-_gray hover:bg-[#fcfcfc] rounded-2xl w-full'>
+            {popular && <div className='absolute -top-5 text-xs font-semibold bg-_primary px-6 py-3 rounded-full'>Popular</div>}
+            <div className='flex flex-col items-center justify-center gap-4 w-full'>
+                <h1 className='font-semibold text-xl'>{name}</h1>
+                <div>
+                    <h1 className='font-semibold text-6xl'>{price}</h1>
+                    {duration && <h1 className='font-semibold text-base text-end'>{duration}</h1>}
+                </div>
+                <Balancer className='text-center'>
+                    {description}
+                </Balancer>
+            </div>
+            <Button variant={'outline'} className={clsx('mt-4 w-full', {
+                'bg-_primary hover:bg-_primary/80 border-_primary': popular,
+                'hover:bg-_gray': !popular,
+            })}>{button}</Button>
+        </div>
+    )
+}
+
+export {
+    GeneralCard,
+    PricingCard,
 }
