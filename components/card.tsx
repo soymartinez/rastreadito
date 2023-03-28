6b957a5a765d6507efd2c90d354d81417cb68fe0
omgismartinez <martnzomg@gmail.com>
2023-03-28 10:19:01 -0600

    🌌 refactor (card): `PricingCard` dark mode

--- components/card.tsx
+++ components/card.tsx
@@ -51,21 +51,21 @@ function PricingCard({
     popular?: boolean,
 }) {
     return (
-        <div className='flex flex-col justify-between relative items-center gap-4 p-12 border-2 border-_gray hover:bg-[#fcfcfc] rounded-2xl w-full'>
-            {popular && <div className='absolute -top-5 text-xs font-semibold bg-_primary px-6 py-3 rounded-full'>Popular</div>}
+        <div className='flex flex-col justify-between relative items-center gap-4 p-12 border-2 border-_gray dark:border-_darkText hover:bg-[#fcfcfc] dark:hover:bg-[#232323] rounded-2xl w-full'>
+            {popular && <div className='absolute -top-5 text-xs font-semibold bg-_primary dark:text-_dark px-6 py-3 rounded-full'>Popular</div>}
             <div className='flex flex-col items-center justify-center gap-4 w-full'>
                 <h1 className='font-semibold text-xl'>{name}</h1>
                 <div>
                     <h1 className='font-semibold text-6xl'>{price}</h1>
                     {duration && <h1 className='font-semibold text-base text-end'>{duration}</h1>}
                 </div>
-                <Balancer className='text-center'>
+                <Balancer className='text-center text-_darkText dark:text-_grayText'>
                     {description}
                 </Balancer>
             </div>
             <Button variant={'outline'} className={clsx('mt-4 w-full', {
-                'bg-_primary hover:bg-_primary/80 border-_primary': popular,
-                'hover:bg-_gray': !popular,
+                'bg-_primary border-_primary dark:text-_dark hover:border-none': popular,
+                'hover:bg-_gray dark:hover:bg-_darkText dark:border-_darkText': !popular,
             })}>{button}</Button>
         </div>
     )
