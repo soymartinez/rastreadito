9149f5e65871de0445e27af2de23071a23c70327
omgismartinez <martnzomg@gmail.com>
2023-03-29 19:36:24 -0600

    🌌 refactor (card): `GeneralCard` responsive

--- components/card.tsx
+++ components/card.tsx
@@ -14,14 +14,16 @@ function GeneralCard({ props, layoutGrid }: {
     layoutGrid: 'layout-list' | 'layout-grid',
 }) {
     return (
-        <div className={clsx('grid mx-auto gap-4 justify-start items-center bg-_dark dark:bg-_darkText hover:bg-_dark/95 dark:hover:bg-_darkText/80 transition-all cursor-pointer p-4 rounded-2xl', {
-            'grid-flow-col': layoutGrid === 'layout-list',
+        <div className={clsx('grid items-center gap-4 bg-_dark dark:bg-_darkText hover:bg-_dark/95 dark:hover:bg-_darkText/80 transition-all cursor-pointer p-4 rounded-2xl', {
+            'grid-flow-col sm:flex': layoutGrid === 'layout-list',
             'grid-cols-2': layoutGrid === 'layout-grid',
         })}>
-            {props.icon === 'active' && <QrCode size={56} className='text-_primary' />}
-            {props.icon === 'use' && <Package size={56} className='text-_primary' />}
-            {props.icon === 'destroy' && <PackageOpen size={56} className='text-_primary' />}
-            <div className={clsx('', {
+            <span>
+                {props.icon === 'active' && <QrCode size={56} className='text-_primary' />}
+                {props.icon === 'use' && <Package size={56} className='text-_primary' />}
+                {props.icon === 'destroy' && <PackageOpen size={56} className='text-_primary' />}
+            </span>
+            <div className={clsx('w-full', {
                 'col-span-full': layoutGrid === 'layout-grid',
             })}>
                 <h1 className='text-xl font-semibold text-_white'>{props.title}</h1>
