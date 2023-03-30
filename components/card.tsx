ac96d49c343f10e84513bbd2e4a6088b5a8e7125
omgismartinez <martnzomg@gmail.com>
2023-03-29 18:56:13 -0600

    🚀 feat (card): `CategoryCard`

--- components/card.tsx
+++ components/card.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx'
 import { Package, PackageOpen, QrCode } from 'lucide-react'
 import Balancer from 'react-wrap-balancer'
 import { Button } from '@/ui/button'
+import Image from 'next/image'
 
 function GeneralCard({ props, layoutGrid }: {
     props: {
@@ -71,7 +72,23 @@ function PricingCard({
     )
 }
 
+function CategoryCard({
+    name,
+    icon,
+}: {
+    name: string,
+    icon: 'dropper' | 'bottle' | 'mortar' | 'cart' | 'blood-drop',
+}) {
+    return (
+        <div className='bg-_dark hover:bg-_dark/95 dark:bg-_darkText dark:hover:bg-_darkText/80 transition-all p-4 overflow-auto rounded-2xl flex gap-4 justify-between items-center cursor-pointer'>
+            <h1 className='text-xl font-semibold text-_white'>{name}</h1>
+            <Image src={`/categories/${icon}.png`} alt={icon} width={44} height={44} />
+        </div>
+    )
+}
+
 export {
     GeneralCard,
     PricingCard,
+    CategoryCard,
 }
