9a70e72cd1271ef4f1fc581d92936a8ceb06e5ab
omgismartinez <martnzomg@gmail.com>
2023-04-01 23:50:31 -0600

    🌌 refactor (layout): wrap app `SupabaseProvider`

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Metadata } from 'next'
 import { Poppins as Font } from 'next/font/google'
 import { ThemeProvider } from '@/components/provider'
+import SupabaseProvider from '@/components/supabase-provider'
 
 const font = Font({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -17,16 +18,12 @@ export const metadata: Metadata = {
   description: 'Rastreadito te permite seguir el rastro de tus productos cannábicos desde su origen hasta tu mano. Descubre su metadata y conoce toda su historia con esta app.',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default async function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={`bg-_white text-_dark dark:bg-_dark dark:text-_white ${font.variable} font-sans`}>
         <ThemeProvider>
-          {children}
+          <SupabaseProvider>{children}</SupabaseProvider>
         </ThemeProvider>
       </body>
     </html>
