caba23b0ecd290efaf4374a64de552fd633632c8
omgismartinez <martnzomg@gmail.com>
2023-04-02 01:48:49 -0600

    🚀 feat: toast

---
+++ components/toast.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { Toaster } from 'sonner'
+
+export default function Toast() {
+    return (
+        <Toaster
+            toastOptions={{
+                style: {
+                    background: '#00e99e',
+                    borderRadius: '1rem',
+                    border: 'none',
+                    fontWeight: 600,
+                },
+            }}
+            closeButton
+        />
+    )
+}
