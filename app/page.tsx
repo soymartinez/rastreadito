ecef61f3d3afa8f0f304744e9ed8ca746b4c6a33
omgismartinez <martnzomg@gmail.com>
2023-04-04 22:48:30 -0600

    🌌 refactor (home): SSR

--- app/page.tsx
+++ app/page.tsx
@@ -1,128 +1,33 @@
-'use client'
-
-import { useState } from 'react'
 import Link from 'next/link'
-import clsx from 'clsx'
-import { LayoutGrid, LayoutList, QrCode } from 'lucide-react'
-import { CategoryCard, GeneralCard } from '@/components/card'
-import Navbar from '@/components/navbar'
-import { Button } from '@/ui/button'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/ui/tabs'
-import Image from 'next/image'
+import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { headers, cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
-  const [layoutGrid, setLayoutGrid] = useState<'layout-list' | 'layout-grid'>('layout-list')
-  const handleLayoutGrid = () => {
-    layoutGrid === 'layout-list'
-      ? setLayoutGrid('layout-grid')
-      : setLayoutGrid('layout-list')
+import { QrCode } from 'lucide-react'
+
+import Navbar from '@/components/navbar'
+import MainTabs from '@/components/main-tabs'
+import { Button } from '@/ui/button'
+
+export const revalidate = 0
+
+export default async function Home() {
+  const supabase = createServerComponentSupabaseClient({
+    headers,
+    cookies,
+  })
+
+  const { data: { session }, error } = await supabase.auth.getSession()
+
+  if (error) {
+    return redirect('/auth')
   }
 
   return (
     <main className='px-4 min-h-screen relative max-w-7xl mx-auto'>
       <Navbar />
-      <h1 className='text-5xl font-bold leading-loose truncate'>BeeHealthy</h1>
-      <Tabs defaultValue='general'>
-        <TabsList className='flex py-2 overflow-x-auto'>
-          <div className='flex gap-2 w-min'>
-            <TabsTrigger
-              value='general'
-              className={`w-28 h-12
-            flex justify-center items-center transition-all
-            data-[state=active]:bg-[#1b1b1b] data-[state=active]:text-_white data-[state=active]:border-none 
-            dark:data-[state=active]:bg-_primary dark:data-[state=active]:text-_dark 
-            bg-_white text-_dark border-2 hover:bg-_gray border-_gray font-[500] rounded-full
-            dark:bg-_dark dark:text-_white dark:border-_darkText dark:hover:bg-_darkText`}
-            >
-              Inicio
-            </TabsTrigger>
-            <TabsTrigger
-              value='categories'
-              className={`w-28 h-12
-            flex justify-center items-center transition-all
-            data-[state=active]:bg-[#1b1b1b] data-[state=active]:text-_white data-[state=active]:border-none 
-            dark:data-[state=active]:bg-_primary dark:data-[state=active]:text-_dark 
-            bg-_white text-_dark border-2 hover:bg-_gray border-_gray font-[500] rounded-full
-            dark:bg-_dark dark:text-_white dark:border-_darkText dark:hover:bg-_darkText`}
-            >
-              Categoría
-            </TabsTrigger>
-            <Link href={'/history'}>
-              <div className={`w-28 h-12
-            flex justify-center items-center transition-all
-            bg-_white text-_dark border-2 hover:bg-_gray border-_gray font-[500] rounded-full
-            dark:bg-_dark dark:text-_white dark:border-_darkText dark:hover:bg-_darkText`}
-              >
-                Historial
-              </div>
-            </Link>
-          </div>
-        </TabsList>
-        <TabsContent value='general'>
-          <div className='flex justify-between items-center pt-6 pb-3'>
-            <h1 className='font-semibold text-xl'>General</h1>
-            <button onClick={handleLayoutGrid}
-              className='p-2 border-2 border-_gray dark:border-_darkText rounded-full'>
-              {layoutGrid === 'layout-list'
-                ? <LayoutGrid />
-                : <LayoutList />}
-            </button>
-          </div>
-          <div className={clsx('grid gap-3', {
-            'grid-cols-1': layoutGrid === 'layout-list',
-            'grid-cols-2': layoutGrid === 'layout-grid',
-          })}>
-            <GeneralCard
-              layoutGrid={layoutGrid}
-              props={{
-                title: 'Activo',
-                description: 'El código aún no ha sido escaneado y utilizado para su propósito previsto.',
-                number: 86,
-                icon: 'active',
-              }}
-            />
-            <GeneralCard
-              layoutGrid={layoutGrid}
-              props={{
-                title: 'Uso',
-                description: 'El código ha sido utilizado y ya no es válido. Evitar el fraude o la duplicación de códigos.',
-                number: 62,
-                icon: 'use',
-              }}
-            />
-            <GeneralCard
-              layoutGrid={layoutGrid}
-              props={{
-                title: 'Destruido',
-                description: 'Un QR destruido no puede ser utilizado y su estado indica que ha sido anulado.',
-                number: 24,
-                icon: 'destroy',
-              }}
-            />
-          </div>
-        </TabsContent>
-        <TabsContent value='categories'>
-          <div className='flex justify-between items-center pt-6 pb-3'>
-            <h1 className='font-semibold text-xl'>Categoria</h1>
-            <button onClick={handleLayoutGrid}
-              className='p-2 border-2 border-_gray dark:border-_darkText rounded-full'>
-              {layoutGrid === 'layout-list'
-                ? <LayoutGrid />
-                : <LayoutList />}
-            </button>
-          </div>
-          <div className={clsx('grid gap-3', {
-            'grid-cols-1': layoutGrid === 'layout-list',
-            'grid-cols-2': layoutGrid === 'layout-grid',
-          })}>
-            <CategoryCard name='Goteo' icon='dropper' />
-            <CategoryCard name='Ungüento' icon='bottle' />
-            <CategoryCard name='Edible' icon='mortar' />
-            <CategoryCard name='Cartucho' icon='cart' />
-            <CategoryCard name='Aceite' icon='blood-drop' />
-          </div>
-        </TabsContent>
-      </Tabs>
+      <h1 className='text-5xl font-bold leading-loose truncate'>{session?.user.email}</h1>
+      <MainTabs />
       <Link href={'/metadata'} className='rounded-2xl'>
         <Button className='w-16 fixed xl:absolute right-4 bottom-8'>
           <QrCode />
