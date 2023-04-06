853025e8af598ca7ae6a9eb22b9c5f8a59f761c3
omgismartinez <martnzomg@gmail.com>
2023-04-05 23:40:01 -0600

    🌌 refactor (home): redirecting unauthenticated users + `user_metadata`

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
-import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { headers, cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 import { QrCode } from 'lucide-react'
+
+import { useCurrentUser } from '@/hooks/auth'
 
 import Navbar from '@/components/navbar'
 import MainTabs from '@/components/main-tabs'
@@ -12,21 +12,12 @@ import { Button } from '@/ui/button'
 export const revalidate = 0
 
 export default async function Home() {
-  const supabase = createServerComponentSupabaseClient({
-    headers,
-    cookies,
-  })
-
-  const { data: { session }, error } = await supabase.auth.getSession()
-
-  if (error) {
-    return redirect('/auth')
-  }
-
+  const user = await useCurrentUser()
+  if (!user) redirect('/auth')
   return (
     <main className='px-4 min-h-screen relative max-w-7xl mx-auto'>
       <Navbar />
-      <h1 className='text-5xl font-bold leading-loose truncate'>{session?.user.email}</h1>
+      <h1 className='text-5xl font-bold leading-loose truncate'>{user.user_metadata.name}</h1>
       <MainTabs />
       <Link href={'/metadata'} className='rounded-2xl'>
         <Button className='w-16 fixed xl:absolute right-4 bottom-8'>
