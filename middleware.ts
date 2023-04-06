5daefb6770bb2d79cf8d898b0ad63052dabbb906
omgismartinez <martnzomg@gmail.com>
2023-04-06 14:15:33 -0600

    🌌 refactor: protected routes

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,27 @@ import type { NextRequest } from 'next/server'
 export async function middleware(req: NextRequest) {
     const res = NextResponse.next()
     const supabase = createMiddlewareSupabaseClient({ req, res })
-    await supabase.auth.getSession()
+    const {
+        data: { session },
+    } = await supabase.auth.getSession()
+
+    const protectedRoutes = [
+        '/',
+        '/account',
+        '/metadata',
+    ]
+
+    if (!session && protectedRoutes.includes(req.nextUrl.pathname)) {
+        const url = new URL(req.url)
+        url.pathname = '/auth'
+        return NextResponse.redirect(url)
+    }
+
+    if (session && req.nextUrl.pathname === '/auth') {
+        const url = new URL(req.url)
+        url.pathname = '/'
+        return NextResponse.redirect(url)
+    }
+
     return res
 }
