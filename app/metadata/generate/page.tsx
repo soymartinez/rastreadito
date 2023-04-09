fbe3dcdc3da495bfd84dd5df75bd3441b784e0a0
omgismartinez <martnzomg@gmail.com>
2023-04-08 18:52:08 -0600

    🌌 refactor (generate): server component + `ModalButton`

--- app/metadata/generate/page.tsx
+++ app/metadata/generate/page.tsx
@@ -1,19 +1,16 @@
-'use client'
+import Image from 'next/image'
+import Balancer from 'react-wrap-balancer'
+import { Download, Printer, QrCode, Send } from 'lucide-react'
 
 import { Back } from '@/ui/back'
 import { Button } from '@/ui/button'
-import { Download, Printer, QrCode, Send } from 'lucide-react'
-import React, { useState } from 'react'
-import Image from 'next/image'
-import Balancer from 'react-wrap-balancer'
 
-import Modal from '@/components/modal'
+import ModalButton from '@/components/modal/modal-button'
 
-export default function Generate() {
-    const [modal, setModal] = useState(false)
+export default async function Generate() {
     return (
         <main className='min-h-screen relative max-w-7xl mx-auto'>
-            <div className='px-4 '>
+            <div className='px-4'>
                 <div className='flex justify-center items-center py-8 relative'>
                     <Back className='absolute left-0' />
                     <h1 className='font-bold text-xl'>QR generado</h1>
@@ -27,44 +24,39 @@ export default function Generate() {
                     <h3 className='font-semibold text-4xl text-_darkText dark:text-_grayText'>Orden</h3>
                     <h1 className='font-semibold text-[110px] text-_dark dark:text-_primary'>#89</h1>
                 </div>
-                <Button onClick={() => setModal(true)} className='w-16 fixed xl:absolute right-4 bottom-8'>
-                    <QrCode />
-                </Button>
             </div>
 
-            {modal && (
-                <Modal onClose={() => setModal(false)}>
-                    <div className='flex flex-col items-center py-16 px-2'>
-                        <div className='w-20 h-20 bg-[#00DB94] rounded-lg flex justify-center items-center object-contain'>
-                            <Image src='/cart-mango-96.png' alt='cart-mango-96' width={63} height={63} />
-                        </div>
-                        <div className='flex flex-col justify-center items-center w-full text-_darkText dark:text-_white font-medium pt-8 pb-10'>
-                            <h1 className='font-semibold text-4xl text-center'>Purple Kush</h1>
-                            <Balancer className='text-center dark:text-_grayText'>
-                                Una cepa híbrida de cannabis, conocida por su aroma a tierra y sus efectos relajantes.
-                                <p>
-                                    Fecha: <span className='font-semibold'>03/03/2023</span>
-                                </p>
-                            </Balancer>
-                        </div>
-                        <QrCode className='text-_dark dark:text-_primary' size={250} />
-                        <div className='flex gap-4 mt-8'>
-                            <Button variant={'outline'}
-                                className='w-16 h-16 dark:border-[#474747] hover:bg-_primary hover:border-_primary dark:hover:border-_primary hover:text-_dark duration-75'>
-                                <Printer />
-                            </Button>
-                            <Button variant={'outline'}
-                                className='w-16 h-16 dark:border-[#474747] hover:bg-_primary hover:border-_primary dark:hover:border-_primary hover:text-_dark duration-75'>
-                                <Download />
-                            </Button>
-                            <Button variant={'outline'}
-                                className='w-16 h-16 dark:border-[#474747] hover:bg-_primary hover:border-_primary dark:hover:border-_primary hover:text-_dark duration-75'>
-                                <Send />
-                            </Button>
-                        </div>
+            <ModalButton icon='QrCode'>
+                <div className='flex flex-col items-center py-16 px-2'>
+                    <div className='w-20 h-20 bg-[#00DB94] rounded-lg flex justify-center items-center object-contain'>
+                        <Image src='/cart-mango-96.png' alt='cart-mango-96' width={63} height={63} />
                     </div>
-                </Modal>
-            )}
+                    <div className='flex flex-col justify-center items-center w-full text-_darkText dark:text-_white font-medium pt-8 pb-10'>
+                        <h1 className='font-semibold text-4xl text-center'>Purple Kush</h1>
+                        <Balancer className='text-center dark:text-_grayText'>
+                            Una cepa híbrida de cannabis, conocida por su aroma a tierra y sus efectos relajantes.
+                            <p>
+                                Fecha: <span className='font-semibold'>03/03/2023</span>
+                            </p>
+                        </Balancer>
+                    </div>
+                    <QrCode className='text-_dark dark:text-_primary' size={250} />
+                    <div className='flex gap-4 mt-8'>
+                        <Button variant={'outline'}
+                            className='w-16 h-16 dark:border-[#474747] hover:bg-_primary hover:border-_primary dark:hover:border-_primary hover:text-_dark duration-75'>
+                            <Printer />
+                        </Button>
+                        <Button variant={'outline'}
+                            className='w-16 h-16 dark:border-[#474747] hover:bg-_primary hover:border-_primary dark:hover:border-_primary hover:text-_dark duration-75'>
+                            <Download />
+                        </Button>
+                        <Button variant={'outline'}
+                            className='w-16 h-16 dark:border-[#474747] hover:bg-_primary hover:border-_primary dark:hover:border-_primary hover:text-_dark duration-75'>
+                            <Send />
+                        </Button>
+                    </div>
+                </div>
+            </ModalButton>
         </main>
     )
 }
