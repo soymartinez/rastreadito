02e794ba33347a44f6744ff808630f9eec31e962
omgismartinez <martnzomg@gmail.com>
2023-04-08 18:31:50 -0600

    🚀 feat (api): qr data fetching

--- app/api/qr/route.ts
+++ app/api/qr/route.ts
@@ -5,3 +5,19 @@ export async function GET(request: Request) {
   const qr = await prisma.qr.findMany()
   return NextResponse.json(qr)
 }
+
+export async function POST(request: Request) {
+  const {
+    idProducto,
+    codigo,
+  } = await request.json()
+
+  const res = await prisma.qr.create({
+    data: {
+      Productos: { connect: { id: idProducto } },
+      codigo,
+      status: 'ACTIVO',
+    },
+  })
+  return NextResponse.json(res)
+}
