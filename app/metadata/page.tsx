a69217b16bbade274203085ccc64097769227ce6
omgismartinez <martnzomg@gmail.com>
2023-04-08 18:28:15 -0600

    🌌 refactor (metadata): create metadata + qr

--- app/metadata/page.tsx
+++ app/metadata/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { useCallback } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { Productos, Qr } from '@prisma/client'
+import { toast } from 'sonner'
+import qs from 'query-string'
 
 import { Info, Save } from 'lucide-react'
 
@@ -18,42 +21,84 @@ import { Textarea } from '@/ui/textarea'
 import { Button } from '@/ui/button'
 
 const productTypes = {
-    flower: 'Flor de cannabis',
-    tincture: 'Goteros de cannabis',
-    ointment: 'Ungüento de cannabis',
-    edible: 'Edibles de cannabis',
-    cartridge: 'Carts de cannabis',
-    oil: 'Aceite de cannabis',
-}
-
-export interface MetadataProps {
-    name: string
-    description: string
-    type: typeof productTypes
-    strain: string
-    thc: string
-    cbd: string
-    flavor: string
-    effect: string
+    FLOR: 'Flor de cannabis',
+    GOTEO: 'Goteros de cannabis',
+    UNGUENTO: 'Ungüento de cannabis',
+    EDIBLES: 'Edibles de cannabis',
+    CARTUCHO: 'Carts de cannabis',
+    ACEITE: 'Aceite de cannabis',
 }
 
 export default function Metadata() {
-    const [value, setValue] = useState<MetadataProps>()
-    const { push } = useRouter()
+    const router = useRouter()
+    const params = useSearchParams()
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         const formData = new FormData(e.target as HTMLFormElement)
-        const data = Object.fromEntries(formData.entries()) as object as MetadataProps
+        const data = Object.fromEntries(formData.entries()) as object as Productos
 
-        setValue(data)
+        const res = async () => {
+            const create = await fetch('/api/metadata', {
+                method: 'POST',
+                body: JSON.stringify(data),
+            })
+
+            if (create.status !== 200) {
+                throw new Error('Error al guardar')
+            }
+
+            return await create.json()
+        }
+
+        const qr = async (data: Productos) => {
+            const codigo = generateQuery(data)
+
+            if (!codigo) {
+                throw new Error('Faltan datos para generar código QR')
+            }
+
+            const create = await fetch('/api/qr', {
+                method: 'POST',
+                body: JSON.stringify({ codigo, idProducto: data.id }),
+            })
+
+            return await create.json()
+        }
+
+        toast.promise(res, {
+            loading: 'Guardando...',
+            success: (producto: Productos) => {
+                toast.promise(qr(producto), {
+                    loading: 'Generando código QR...',
+                    success: (qr: Qr) => {
+                        router.push(qr.codigo)
+                        return <div>Código QR <strong>#{qr.id}</strong> generado</div>
+                    },
+                    error: 'Error al generar código QR',
+                })
+                return <div>Producto <strong>{producto.nombre}</strong> guardado</div>
+            },
+            error: 'Error al guardar',
+        })
     }
 
-    useEffect(() => {
-        if (value) {
-            push('/metadata/generate')
+    const generateQuery = useCallback((data: Productos) => {
+        const updatedQuery: any = {
+            fecha: data.createdAt,
+            id: data.id,
+            nombre: data.nombre,
+            tipo: data.tipo,
         }
-    }, [value, push])
+
+        const url = qs.stringifyUrl({
+            url: '/metadata/generate',
+            query: updatedQuery
+        }, { skipNull: true })
+
+        return url
+    }, [router, params])
+
     return (
         <div className='px-4 min-h-screen relative max-w-7xl mx-auto'>
             <div className='flex justify-center items-center py-8 relative'>
@@ -67,9 +112,9 @@ export default function Metadata() {
                         <Info />
                     </div>
                     <div className='flex flex-col gap-3'>
-                        <Input name='name' labelText='Nombre' placeholder='Purple Kush' required />
-                        <Textarea name='description' labelText='Descripción' placeholder='Purple Kush' required />
-                        <Select name='type' required>
+                        <Input name='nombre' labelText='Nombre' placeholder='Purple Kush' required />
+                        <Textarea name='descripcion' labelText='Descripción' placeholder='Purple Kush' required />
+                        <Select name='tipo' required>
                             <SelectTrigger labelText='Tipo de producto'>
                                 <SelectValue placeholder='Selecciona un tipo de producto' />
                             </SelectTrigger>
@@ -89,11 +134,11 @@ export default function Metadata() {
                         <Info />
                     </div>
                     <div className='flex flex-col gap-3'>
-                        <Input name='strain' labelText='Cepa' placeholder='Purple Kush' required />
+                        <Input name='cepa' labelText='Cepa' placeholder='Purple Kush' required />
                         <Input name='thc' labelText='THC' placeholder='0.0' required variant='porcentage' />
                         <Input name='cbd' labelText='CBD' placeholder='0.0' required variant='porcentage' />
-                        <Input name='flavor' labelText='Aroma' placeholder='Tierra, pino, dulce' required />
-                        <Input name='effects' labelText='Efectos' placeholder='Relajante, calmante, eufórico' required />
+                        <Input name='aroma' labelText='Aroma' placeholder='Tierra, pino, dulce' required />
+                        <Input name='efecto' labelText='Efectos' placeholder='Relajante, calmante, eufórico' required />
                     </div>
                 </div>
                 <Button type='submit' className='w-16 fixed xl:absolute right-4 bottom-8'>
