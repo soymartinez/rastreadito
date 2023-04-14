bc92815fe364e97585a2588e1e99b06bb88044a2
omgismartinez <martnzomg@gmail.com>
2023-04-13 22:59:00 -0600

    🚀 feat (qr/usuario): get

---
+++ app/api/qr/[usuario]/route.ts
@@ -0,0 +1,13 @@
+import { prisma } from "@/lib/prisma"
+import { NextResponse } from "next/server"
+
+export async function GET(request: Request, { params }: { params: { usuario: string } }) {
+    const qr = await prisma.qr.findMany({
+        where: {
+            producto: {
+                usuario: params.usuario
+            }
+        },
+    })
+    return NextResponse.json(qr)
+}
