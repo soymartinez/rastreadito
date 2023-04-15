7d7fb97462ee7c37b937a80afb8f380ab1413537
omgismartinez <martnzomg@gmail.com>
2023-04-15 13:28:52 -0600

    🌌 refactor (history): get history by user

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -2,50 +2,29 @@ import { Metadata } from 'next'
 import { Back } from '@/ui/back'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/ui/tabs'
 import { Check, Tag, X } from 'lucide-react'
+import { getCurrentUser } from '@/hooks/auth'
+import { Producto, Qr } from '@prisma/client'
+import { headers } from 'next/headers'
 
 export const metadata: Metadata = {
     title: 'Historial',
     description: 'Administra el historial de tus productos.',
 }
 
-export default function History() {
-    const data = [
-        {
-            id: 1,
-            invoice: '#cart 86',
-            client: 'Bee',
-            date: '12/12/2021',
-            status: 'active',
-        },
-        {
-            id: 2,
-            invoice: '#flower 67',
-            client: 'Bee',
-            date: '12/12/2021',
-            status: 'destroy',
-        },
-        {
-            id: 3,
-            invoice: '#oil 12',
-            client: 'Bee',
-            date: '12/12/2021',
-            status: 'active',
-        },
-        {
-            id: 4,
-            invoice: '#cart 86',
-            client: 'Bee',
-            date: '12/12/2021',
-            status: 'active',
-        },
-        {
-            id: 5,
-            invoice: '#flower 67',
-            client: 'Bee',
-            date: '12/12/2021',
-            status: 'use',
-        },
-    ]
+type QrProductType = (Qr & { producto: Producto })[]
+
+export async function getHistorial(origin: string, usuario: string): Promise<QrProductType> {
+    const res = await fetch(origin + '/api/qr/' + usuario)
+    const data = await res.json()
+    return data
+}
+
+export default async function History() {
+    const { get } = headers()
+    const origin = get('x-origin') || ''
+
+    const usuario = await getCurrentUser()
+    const historial = await getHistorial(origin, usuario?.email || '')
 
     const ActiveButton = () => (
         <div className='bg-_primary/[15%] text-_primary flex items-center justify-center gap-1 w-min rounded-full px-3 py-1'>
@@ -70,7 +49,7 @@ export default function History() {
                 <Back className='absolute left-0' />
                 <h1 className='font-bold text-xl'>Historial</h1>
             </div>
-            <h1 className='text-5xl font-bold leading-loose truncate'>BeeHealthy</h1>
+            <h1 className='text-5xl font-bold leading-loose truncate'>{usuario?.user_metadata.name}</h1>
             <Tabs defaultValue='all'>
                 <TabsList className='py-2 overflow-x-auto'>
                     <div className='flex gap-2 w-min'>
@@ -114,27 +93,27 @@ export default function History() {
                             </tr>
                         </thead>
                         <tbody className='text-_grayText text-base overflow-hidden'>
-                            {data.map((item) => (
+                            {historial.map(({ id, estatus, producto: { id: productoId, categoria, usuario }, fechaRegistro }) => (
                                 <tr
-                                    key={item.id}
-                                    className='bg-_white hover:bg-_gray/50 dark:bg-_dark overflow-x-auto'
+                                    key={id}
+                                    className='bg-_white hover:bg-_gray/50 dark:bg-_dark dark:hover:bg-_darkText/50 overflow-x-auto'
                                 >
                                     <td className='px-3 py-2'>
                                         <div className='flex items-center justify-center'>
                                             <input
                                                 type='checkbox'
-                                                name={`${item.id}`}
-                                                id={`${item.id}`}
+                                                name={`${id}`}
+                                                id={`${id}`}
                                                 className='w-4 h-4 m-auto accent-_primary rounded-full' />
                                         </div>
                                     </td>
-                                    <td className='px-3 py-2 font-semibold whitespace-nowrap text-_dark dark:text-_white uppercase'>{item.invoice}</td>
-                                    <td className='px-3 py-2 font-semibold whitespace-nowrap'>{item.client}</td>
-                                    <td className='px-3 py-2 font-semibold'>{item.date}</td>
+                                    <td className='px-3 py-2 font-semibold whitespace-nowrap text-_dark dark:text-_white uppercase'>#{categoria} {productoId}</td>
+                                    <td className='px-3 py-2 font-semibold whitespace-nowrap'>{usuario}</td>
+                                    <td className='px-3 py-2 font-semibold'>{new Date(fechaRegistro).toLocaleString(undefined, { hour12: true })}</td>
                                     <td className='px-3 py-2 font-semibold sticky right-0 z-20 bg-inherit backdrop-blur-md border-l-4 border-_gray dark:border-_darkText'>
-                                        {item.status === 'active' && <ActiveButton />}
-                                        {item.status === 'use' && <UseButton />}
-                                        {item.status === 'destroy' && <DestroyButton />}
+                                        {estatus === 'ACTIVO' && <ActiveButton />}
+                                        {estatus === 'USADO' && <UseButton />}
+                                        {estatus === 'DESTRUIDO' && <DestroyButton />}
                                     </td>
                                 </tr>
                             ))}
