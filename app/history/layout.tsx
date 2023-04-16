0b36adc067febb51339b4d2ddb91ef440812075b
omgismartinez <martnzomg@gmail.com>
2023-04-15 23:02:31 -0600

    🚀 feat (history): layout support intercepting routes

---
+++ app/history/layout.tsx
@@ -0,0 +1,19 @@
+export const metadata = {
+    title: 'Historial',
+    description: 'Administra el historial de tus productos.',
+}
+
+export default function HistoryLayout({
+    children,
+    modal,
+}: {
+    children: React.ReactNode;
+    modal: React.ReactNode;
+}) {
+    return (
+        <main>
+            {children}
+            {modal}
+        </main>
+    )
+}
