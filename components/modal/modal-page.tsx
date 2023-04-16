4d1b66e6b9ec8b95bc79af4310468d579f34064a
omgismartinez <martnzomg@gmail.com>
2023-04-15 22:59:32 -0600

    🚀 feat (modal): modal page

---
+++ components/modal/modal-page.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+} from '@/ui/dialog'
+import { Maximize2 } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+
+interface ModalPageProps {
+    children: React.ReactNode
+}
+
+export default function ModalPage({ children }: ModalPageProps) {
+    const { back } = useRouter()
+    return (
+        <Dialog
+            defaultOpen
+            onOpenChange={(open) => open ? null : back()}
+        >
+            <DialogContent className='sm:max-w-3xl sm:h-[600px] overflow-hidden flex flex-col justify-between'>
+                <div className='flex justify-between sticky top-0 h-min'>
+                    <button
+                        onClick={() => window.location.reload()}
+                        className='w-min dark:bg-_dark dark:hover:bg-_primary/[15%] p-2 rounded-full'
+                    >
+                        <Maximize2 className='h-4 w-4 dark:text-_primary' />
+                    </button>
+                    <DialogClose />
+                </div>
+                <div className='h-full overflow-auto'>
+                    {children}
+                </div>
+            </DialogContent>
+        </Dialog >
+    )
+}
