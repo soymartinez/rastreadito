c67e084e0c216959dfd3346dc54ed4aacef9deda
omgismartinez <martnzomg@gmail.com>
2023-04-17 16:22:31 -0600

    🌌 refactor (modal-page): dark mode support

--- components/modal/modal-page.tsx
+++ components/modal/modal-page.tsx
@@ -23,7 +23,17 @@ export default function ModalPage({ children }: ModalPageProps) {
                 <div className='flex justify-between sticky top-0 h-min'>
                     <button
                         onClick={() => window.location.reload()}
-                        className='w-min dark:bg-_dark dark:hover:bg-_primary/[15%] p-2 rounded-full'
+                        className='
+                            w-min
+                            transition
+                            dark:bg-_dark
+                            bg-_primary
+                            hover:bg-_primary/80
+                            dark:hover:bg-_primary/30
+                            dark:bg-_primary/[15%]
+                            p-2
+                            rounded-full
+                        '
                     >
                         <Maximize2 className='h-4 w-4 dark:text-_primary' />
                     </button>
