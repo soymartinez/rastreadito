62e5aaf550908d6af4648eeb69664d8a13733a1f
omgismartinez <martnzomg@gmail.com>
2023-04-20 00:09:32 -0600

    🌌 refactor (history): table component

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,9 +1,8 @@
 import { Back } from '@/ui/back'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/ui/tabs'
 import { getCurrentUser } from '@/hooks/auth'
-import Tr from '@/components/hover/tr'
-import { QrProductType } from '@/types'
 import { prisma } from '@/lib/prisma'
+import Table from './table'
 
 async function getHistorial(usuario: string) {
     const res = await prisma.qr.findMany({
@@ -55,27 +54,7 @@ export default async function History() {
                     </div>
                 </TabsList>
                 <TabsContent value='all' className='overflow-auto w-full'>
-                    <table className='table-auto text-xs w-full border-separate border-spacing-0 my-6'>
-                        <thead className='text-_grayText uppercase sticky top-0 z-30'>
-                            <tr className='text-left'>
-                                <th className='px-3 py-2'>
-                                    <div className='flex items-center justify-center'>
-                                        <input type='checkbox' name='select-all' id='select-all' className='w-4 h-4 m-auto accent-_primary rounded-full' />
-                                    </div>
-                                </th>
-                                <th className='px-3 py-2 font-medium'>Factura</th>
-                                <th className='px-3 py-2 font-medium'>Producto</th>
-                                <th className='px-3 py-2 font-medium'>Cliente</th>
-                                <th className='px-3 py-2 font-medium'>Fecha</th>
-                                <th className='px-3 py-2 font-medium sticky right-0 bg-_white dark:bg-_dark border-l-4 border-_gray dark:border-_darkText'>Estado</th>
-                            </tr>
-                        </thead>
-                        <tbody className='text-_grayText text-base overflow-hidden'>
-                            {historial.map((data: QrProductType) => (
-                                <Tr key={data.id} data={data} />
-                            ))}
-                        </tbody>
-                    </table>
+                    <Table data={historial} />
                 </TabsContent>
                 <TabsContent value='date'>
 
