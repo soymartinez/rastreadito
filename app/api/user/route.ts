7fe4afb26c4fc0aa711f5b9bf23a4f0fc3fd05bc
omgismartinez <martnzomg@gmail.com>
2023-04-21 00:34:00 -0600

    🚀 feat (user): patch metod

---
+++ app/api/user/route.ts
@@ -0,0 +1,20 @@
+import { useSupabaseServer } from '@/hooks/auth'
+import { NextResponse } from 'next/server'
+
+export async function PATCH(request: Request) {
+    const { supabase } = await useSupabaseServer()
+    const {
+        name,
+    } = await request.json()
+    const res = await supabase.auth.updateUser({
+        data: {
+            name,
+        },
+    })
+
+    if (res.error) {
+        return NextResponse.error()
+    }
+
+    return NextResponse.json(res)
+}
