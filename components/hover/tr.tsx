38607d52ba2498355d53e17c7a72ca00bc01216f
omgismartinez <martnzomg@gmail.com>
2023-04-24 23:21:53 -0600

    🌌 refactor (tr): button to enlarge the product

--- components/hover/tr.tsx
+++ components/hover/tr.tsx
@@ -32,25 +32,27 @@ export default function Tr({
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
         >
-            <td className='px-3 py-2'>
-                <div className='flex items-center justify-center'>
-                    <input
-                        type='checkbox'
-                        name={`${id}`}
-                        id={`${id}`}
-                        className='w-4 h-4 m-auto accent-_primary rounded-full' />
+            <td className='pl-3 pr-1 py-2 w-20'>
+                <div className='flex justify-between items-center'>
+                    <div className='flex items-center justify-center'>
+                        <input
+                            type='checkbox'
+                            name={`${id}`}
+                            id={`${id}`}
+                            className='w-4 h-4 m-auto accent-_primary rounded-full' />
+                    </div>
+                    <div>
+                        {hover &&
+                            <Link href={`/product/${productoId}`}>
+                                <div className='bg-_white dark:bg-_dark dark:hover:bg-_primary/[15%] transition p-2 rounded-full'>
+                                    <Maximize2 size={14} className='text-_dark dark:text-_primary' />
+                                </div>
+                            </Link>}
+                    </div>
                 </div>
             </td>
             <td className='px-3 py-2 font-semibold whitespace-nowrap text-_dark dark:text-_white uppercase'>#{categoria} {id}</td>
-            <td className='px-3 py-2 font-semibold whitespace-nowrap relative'>
-                {hover &&
-                    <Link href={`/product/${productoId}`}>
-                        <div className='absolute right-0 top-2.5 bg-_white dark:bg-_dark dark:hover:bg-_primary/[15%] transition p-2 rounded-full'>
-                            <Maximize2 size={14} className='text-_dark dark:text-_primary' />
-                        </div>
-                    </Link>}
-                {productoNombre}
-            </td>
+            <td className='px-3 py-2 font-semibold whitespace-nowrap'>{productoNombre}</td>
             <td className='px-3 py-2 font-semibold whitespace-nowrap'>{usuario}</td>
             <td className='px-3 py-2 font-semibold whitespace-nowrap'>{new Date(fechaRegistro).toLocaleString(undefined, { hour12: true })}</td>
             <td className='px-3 py-2 font-semibold sticky right-0 z-20 bg-inherit backdrop-blur-md border-l-4 border-_gray dark:border-_darkText'>
