e3b61194a13b1c75327f2e251d376738ca6b09f3
omgismartinez <martnzomg@gmail.com>
2023-04-25 01:19:49 -0600

    🌌 refactor (table): select all + select list

--- app/history/table.tsx
+++ app/history/table.tsx
@@ -2,19 +2,44 @@
 
 import Tr from '@/components/hover/tr'
 import { QrProductType } from '@/types'
+import { useState } from 'react'
 
 interface TableProps {
     data: QrProductType[]
 }
 
 export default function Table({ data }: TableProps) {
+    const [selectAll, setSelectAll] = useState(false)
+    const [selectList, setSelectList] = useState<number[]>([])
+
+    const handleSelectedAll = (checked: boolean) => {
+        setSelectAll(checked)
+        checked
+            ? setSelectList(data.map((item) => item.id))
+            : setSelectList([])
+    }
+
+    const handleSelected = (id: number) => {
+        const index = selectList.indexOf(id)
+        index === -1
+            ? setSelectList([...selectList, id])
+            : setSelectList(selectList.filter((item) => item !== id))
+    }
+
     return (
         <table className='table-auto text-xs w-full border-separate border-spacing-0 my-6'>
             <thead className='text-_grayText uppercase sticky top-0 z-30'>
                 <tr className='text-left'>
                     <th className='pl-3 pr-1 py-2 flex justify-between'>
                         <div className='flex items-center justify'>
-                            <input type='checkbox' name='select-all' id='select-all' className='w-4 h-4 m-auto accent-_primary rounded-full' />
+                            <input
+                                onChange={(e) => handleSelectedAll(e.target.checked)}
+                                type='checkbox'
+                                name='select-all'
+                                id='select-all'
+                                checked={selectList.length === data.length}
+                                className='w-4 h-4 m-auto accent-_primary rounded-full'
+                            />
                         </div>
                     </th>
                     <th className='px-3 py-2 font-medium'>Factura</th>
@@ -26,7 +51,12 @@ export default function Table({ data }: TableProps) {
             </thead>
             <tbody className='text-_grayText text-base overflow-hidden'>
                 {data.map((data: QrProductType) => (
-                    <Tr key={data.id} data={data} />
+                    <Tr
+                        key={data.id}
+                        data={data}
+                        defaultChecked={selectAll}
+                        isSelected={(id: number) => handleSelected(id)}
+                    />
                 ))}
             </tbody>
         </table>
