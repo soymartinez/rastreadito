fb67d44718101656bf33ba4772b0f0a40b629d11
omgismartinez <martnzomg@gmail.com>
2023-04-26 14:18:01 -0600

    🌌 refactor (navbar): sticky

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,17 +3,19 @@ import { Scan } from 'lucide-react'
 
 export default function Navbar() {
     return (
-        <nav className='flex justify-between items-center py-6 relative'>
-            <h1 className='font-black text-2xl uppercase italic'>rastreadito</h1>
-            <div className='flex gap-2'>
-                <Link href={'/scan'} className='rounded-full'>
-                    <div className='w-11 h-11 border-2 border-_gray dark:border-_darkText rounded-full flex items-center justify-center'>
-                        <Scan />
-                    </div>
-                </Link>
-                <Link href={'/account'} className='rounded-full'>
-                    <div className='w-11 h-11 border-2 border-_gray dark:border-_darkText bg-_primary rounded-full' />
-                </Link>
+        <nav className='sticky top-0 z-50 bg-white dark:bg-_dark/60 backdrop-blur-[8px]'>
+            <div className='flex justify-between items-center py-6 relative px-4 max-w-7xl mx-auto'>
+                <h1 className='font-black text-2xl uppercase italic'>rastreadito</h1>
+                <div className='flex gap-2'>
+                    <Link href={'/scan'} className='rounded-full'>
+                        <div className='w-11 h-11 border-2 border-_gray dark:border-_darkText rounded-full flex items-center justify-center'>
+                            <Scan />
+                        </div>
+                    </Link>
+                    <Link href={'/account'} className='rounded-full'>
+                        <div className='w-11 h-11 border-2 border-_gray dark:border-_darkText bg-_primary rounded-full' />
+                    </Link>
+                </div>
             </div>
         </nav>
     )
