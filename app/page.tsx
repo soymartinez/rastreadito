0f8ef777366976c06bef79b7315212f0a6b1ec54
omgismartinez <martnzomg@gmail.com>
2023-04-26 14:42:54 -0600

    🌌 refactor (home): design structure

--- app/page.tsx
+++ app/page.tsx
@@ -28,19 +28,21 @@ export default async function Home() {
   const user = await getCurrentUser()
   const qr = await getQr(origin, user?.email ?? '')
   return (
-    <main className='px-4 min-h-screen relative max-w-7xl mx-auto'>
+    <main>
       <Navbar />
-      <h1 className='text-5xl font-bold leading-loose truncate'>{user?.user_metadata.name}</h1>
-      <MainTabs
-        qr={qr}
-      />
-      <Link href={'/metadata'} className='rounded-2xl'>
-        <div className='absolute z-30 w-16 right-4'>
-          <Button type='submit' className='w-16 fixed bottom-8'>
-            <QrCode />
-          </Button>
-        </div>
-      </Link>
+      <div className='px-4 min-h-screen relative max-w-7xl mx-auto'>
+        <h1 className='text-5xl font-bold leading-loose truncate'>{user?.user_metadata.name}</h1>
+        <MainTabs
+          qr={qr}
+        />
+        <Link href={'/metadata'} className='rounded-2xl'>
+          <div className='absolute z-30 w-16 right-4'>
+            <Button type='submit' className='w-16 fixed bottom-8'>
+              <QrCode />
+            </Button>
+          </div>
+        </Link>
+      </div>
     </main >
   )
 }
