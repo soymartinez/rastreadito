c342235c18c89308cd0f9cc5a1d18c5b665a3b1e
omgismartinez <martnzomg@gmail.com>
2023-05-01 13:48:33 -0600

    🌌 refactor: domain images exmaple

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: [
+      'leafly-public.imgix.net',
+      'images.leafly.com',
+      'www.leafly.com',
+    ]
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
