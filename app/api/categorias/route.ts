049c8fb37b5e6cb70fc575d9d2f61c592cbae88a
omgismartinez <martnzomg@gmail.com>
2023-05-06 15:49:25 -0600

    🌌 refactor (categorias): order by id

--- app/api/categorias/route.ts
+++ app/api/categorias/route.ts
@@ -2,6 +2,10 @@ import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-    const qr = await prisma.categoria.findMany()
+    const qr = await prisma.categoria.findMany({
+        orderBy: {
+            id: 'asc'
+        }
+    })
     return NextResponse.json(qr)
 }
