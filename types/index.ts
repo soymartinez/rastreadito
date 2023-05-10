ada835d3fa1d16572d600dd832861b354f6fb3c5
omgismartinez <martnzomg@gmail.com>
2023-05-09 22:36:19 -0600

    🌌 refactor (types): added CategoriaGaleriaType

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,10 @@
-import { Producto, Qr } from '@prisma/client'
+import {
+    Categoria,
+    Galeria,
+    Producto,
+    Qr
+} from '@prisma/client'
 
 export type QrProductType = (Qr & { producto: Producto })
+
+export type CategoriaGaleriaType = (Categoria & { galeria: Galeria[] })
