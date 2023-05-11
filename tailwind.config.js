a2e290be9506baa010475f0509b234e4c423657e
omgismartinez <martnzomg@gmail.com>
2023-05-10 23:30:37 -0600

    🌌 refactor: tailwind-scrollbar plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,5 +41,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [
+    require('tailwindcss-animate'),
+    require('tailwind-scrollbar')({ nocompatible: true }),
+  ],
 }
