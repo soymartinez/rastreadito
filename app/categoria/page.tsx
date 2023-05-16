8f30a41f09ddca78d5a6496f0b5b7bc8efc4c683
omgismartinez <martnzomg@gmail.com>
2023-05-15 22:28:16 -0600

    🚀 feat: categoria page

---
+++ app/categoria/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+
+export default function Categoria() {
+    const search = useSearchParams()
+    const tipo = search.get('tipo')
+    return (
+        <div>Categoria page {tipo}</div>
+    )
+}
