040c419e708a2785d06584f8f9d1e522b5a566c7
omgismartinez <martnzomg@gmail.com>
2023-05-18 10:34:22 -0600

    🌌 refactor (image-preview): remove button

--- components/gallery/image-preview.tsx
+++ components/gallery/image-preview.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react'
 import Image from 'next/image'
-import { RotateCcw } from 'lucide-react'
+import { RotateCcw, X } from 'lucide-react'
 import clsx from 'clsx'
 
 interface ImagePreviewProps {
@@ -10,8 +10,10 @@ interface ImagePreviewProps {
     alt: string
     selected?: boolean
     changeIcon?: boolean
+    defaultIcon?: boolean
     className?: string
-    onClick?: () => void
+    onChangeImage?: () => void
+    onRemoveImage?: () => void
 }
 
 export default function ImagePreview({
@@ -19,16 +21,21 @@ export default function ImagePreview({
     alt,
     selected,
     changeIcon = true,
+    defaultIcon = false,
     className,
-    onClick,
+    onChangeImage,
+    onRemoveImage
 }: ImagePreviewProps) {
     const [hover, setHover] = useState(false)
     return (
         <div
-            onClick={onClick}
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
-            className={clsx(`
+            className='relative'
+        >
+            <div
+                onClick={onChangeImage}
+                className={clsx(`
                 relative
                 min-w-[100px]
                 min-h-[100px]
@@ -43,29 +50,44 @@ export default function ImagePreview({
                 rounded-2xl
                 overflow-hidden
             `, className)}
-        >
-            <Image
-                className={clsx('transition-transform object-contain', {
-                    'scale-105': hover,
-                    'scale-100': !hover,
-                })}
-                src={src}
-                alt={alt}
-                blurDataURL={src}
-                placeholder='blur'
-                width={200}
-                height={200}
-            />
-            <div className={clsx('absolute inset-0 transition flex justify-center items-center rounded-2xl overflow-hidden', {
-                'opacity-0 scale-125': !hover,
-                'opacity-100 scale-100 bg-_dark/30': hover,
-                'hidden': !changeIcon,
-            })}>
-                <RotateCcw />
+            >
+                <Image
+                    className={clsx('transition-transform object-contain', {
+                        'scale-105': hover,
+                        'scale-100': !hover,
+                    })}
+                    src={src}
+                    alt={alt}
+                    blurDataURL={src}
+                    placeholder='blur'
+                    width={200}
+                    height={200}
+                />
+                <div className={clsx('absolute inset-0 transition flex justify-center items-center rounded-2xl overflow-hidden', {
+                    'opacity-0 scale-125': !hover,
+                    'opacity-100 scale-100 bg-_dark/30': hover,
+                    'hidden': !changeIcon,
+                })}>
+                    <RotateCcw />
+                </div>
+                <div className={clsx('absolute inset-0 pointer-events-none', {
+                    'bg-_primary bg-opacity-50': selected,
+                })} />
             </div>
-            <div className={clsx('absolute inset-0 pointer-events-none', {
-                'bg-_primary bg-opacity-50': selected,
-            })} />
+            <button
+                onClick={onRemoveImage}
+                style={{
+                    top: '0.3rem',
+                    right: '0.3rem',
+                }}
+                className={clsx('absolute z-50 transition rounded-full p-1 overflow-hidden bg-_primary text-_dark', {
+                    'opacity-0 scale-125': !hover,
+                    'opacity-100 scale-100': hover,
+                    'hidden': !defaultIcon,
+                })}
+            >
+                <X size={16} />
+            </button>
         </div>
     )
 }
