dc95d05ba82adeb0165e7a4e0d1f5be2fe12b695
omgismartinez <martnzomg@gmail.com>
2023-05-20 16:06:19 -0600

    🚀 feat (actions): delete product

---
+++ app/actions/delete-product.ts
@@ -0,0 +1,20 @@
+'use server'
+
+import { prisma } from '@/lib/prisma'
+import { revalidatePath } from 'next/cache'
+
+export async function deleteProduct(ids: number[]) {
+    const res = await prisma.producto.deleteMany({
+        where: {
+            qr: {
+                id: {
+                    in: ids
+                }
+            }
+        }
+    })
+
+ 
+    revalidatePath('/history')
+    return res
+}
