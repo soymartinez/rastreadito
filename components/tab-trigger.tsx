65875882139e20faffc31c85c7faedb5f0c5bb1b
omgismartinez <martnzomg@gmail.com>
2023-05-19 22:08:08 -0600

    🌌 refactor (tab-trigger): bg hover light mode

--- components/tab-trigger.tsx
+++ components/tab-trigger.tsx
@@ -13,7 +13,7 @@ export default function TabTrigger({ value, label, className }: TabTriggerProps)
             value={value}
             className={clsx(`w-28 h-12
                 flex justify-center items-center transition-all
-                data-[state=active]:bg-[#1b1b1b] data-[state=active]:text-_white data-[state=active]:border-none 
+                data-[state=active]:bg-[#1b1b1b] data-[state=active]:hover:bg-_dark data-[state=active]:text-_white data-[state=active]:border-none 
                 dark:data-[state=active]:bg-_primary dark:data-[state=active]:hover:bg-_primary dark:data-[state=active]:text-_dark 
                 bg-_white text-_dark border-2 hover:bg-_gray border-_gray font-[500] rounded-full
                 dark:bg-_dark dark:text-_white dark:border-_darkText dark:hover:bg-_darkText`, className)}
