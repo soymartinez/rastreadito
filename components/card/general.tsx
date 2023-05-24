b5f0d2ce15062ebd2e966973cb9f710d14973f58
omgismartinez <martnzomg@gmail.com>
2023-05-24 15:07:06 -0600

    🌌 refactor (general): estaus prop

--- components/card/general.tsx
+++ components/card/general.tsx
@@ -1,8 +1,10 @@
+import { Estatus } from '@prisma/client'
 import {
     Package,
     PackageOpen,
     QrCode
 } from 'lucide-react'
+import Link from 'next/link'
 
 export default function GeneralCard({ props }: {
     props: {
@@ -10,22 +12,25 @@ export default function GeneralCard({ props }: {
         description: string
         number: number,
         icon: 'active' | 'use' | 'destroy',
+        estatus: Estatus,
     },
 }) {
     return (
-        <div className={'grid grid-flow-col sm:flex items-center gap-4 bg-_dark dark:bg-_darkText hover:bg-_dark/95 dark:hover:bg-_darkText/80 transition-all cursor-pointer p-4 rounded-2xl'}>
-            <span>
-                {props.icon === 'active' && <QrCode size={56} className='text-_primary' />}
-                {props.icon === 'use' && <Package size={56} className='text-_primary' />}
-                {props.icon === 'destroy' && <PackageOpen size={56} className='text-_primary' />}
-            </span>
-            <div className={'w-full'}>
-                <h1 className='text-xl font-semibold text-_white'>{props.title}</h1>
-                <p className='text-xs font-medium text-_grayText'>
-                    {props.description}
-                </p>
+        <Link href={`/history/${props.estatus.toLowerCase()}`}>
+            <div className={'grid grid-flow-col sm:flex items-center gap-4 bg-_dark dark:bg-_darkText hover:bg-_dark/95 dark:hover:bg-_darkText/80 transition-all cursor-pointer p-4 rounded-2xl'}>
+                <span>
+                    {props.icon === 'active' && <QrCode size={56} className='text-_primary' />}
+                    {props.icon === 'use' && <Package size={56} className='text-_primary' />}
+                    {props.icon === 'destroy' && <PackageOpen size={56} className='text-_primary' />}
+                </span>
+                <div className={'w-full'}>
+                    <h1 className='text-xl font-semibold text-_white'>{props.title}</h1>
+                    <p className='text-xs font-medium text-_grayText'>
+                        {props.description}
+                    </p>
+                </div>
+                <h1 className={'text-5xl font-semibold text-_white'}>{props.number}</h1>
             </div>
-            <h1 className={'text-5xl font-semibold text-_white'}>{props.number}</h1>
-        </div>
+        </Link>
     )
 }
