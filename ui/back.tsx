36d4cf33b14f6ce32d1eacf575de22bfb144fc36
omgismartinez <martnzomg@gmail.com>
2023-05-24 23:55:49 -0600

    🌌 refactor (back): revalidate

--- ui/back.tsx
+++ ui/back.tsx
@@ -5,20 +5,24 @@ import React from 'react'
 import { useRouter } from 'next/navigation'
 import { ChevronLeft, LucideProps } from 'lucide-react'
 import clsx from 'clsx'
+import { revalidateAction } from '@/app/actions/revalidate'
 
 interface BackProps extends LucideProps {
-    pushRoute?: string
+    pushRoute?: string,
+    revalidate?: string,
 }
 
 const Back = React.forwardRef<SVGSVGElement, BackProps>(
-    ({ pushRoute, className, ...props }, ref) => {
+    ({ pushRoute, revalidate, className, ...props }, ref) => {
         const { back, push } = useRouter()
         return <ChevronLeft
             ref={ref}
             className={clsx('text-_dark dark:text-_white mx-3 cursor-pointer', className)}
             size={32}
             strokeWidth={3}
-            onClick={() => {
+            onClick={async () => {
+                revalidate && await revalidateAction(revalidate)
+                pushRoute && await revalidateAction(pushRoute)
                 pushRoute
                     ? push(pushRoute)
                     : back()
