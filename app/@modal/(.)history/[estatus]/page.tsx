2b1392e5bea084be6750a3978a40b08f5b25c410
omgismartinez <martnzomg@gmail.com>
2023-05-24 23:09:03 -0600

    🌌 refactor (estatus): qr status history

--- app/@modal/(.)history/[estatus]/page.tsx
+++ app/@modal/(.)history/[estatus]/page.tsx
@@ -23,28 +23,40 @@ const getStatusHistory = async (estatus: Estatus) => {
 
 export default async function HistoryStatus({ params }: { params: { estatus: string } }) {
     const history = await getStatusHistory(params.estatus.toUpperCase() as Estatus)
+
+    const title = () => {
+        const estatus = params.estatus.toUpperCase() as Estatus
+        if (estatus === Estatus.ACTIVO) return 'QRs activos'
+        if (estatus === Estatus.USADO) return 'QRs en uso'
+        if (estatus === Estatus.DESTRUIDO) return 'QRs destruidos'
+    }
+
     return (
         <ModalPage>
-            <div className='sticky top-0 z-50 flex justify-between items-center bg-_white dark:bg-_dark border-b border-_gray dark:border-_darkText'>
-                <h1 className='text-5xl font-bold leading-loose truncate'>{params.estatus}</h1>
+            <div className='sticky top-0 z-50 bg-_white dark:bg-_dark border-b border-_gray dark:border-_darkText'>
+                <h1 className='text-5xl font-bold leading-loose truncate'>{title()}</h1>
             </div>
             <section>
                 {history.length > 0
                     ? <div className='flex flex-col gap-4'>
                         {history.map(({ id, producto }) => (
-                            <div key={id} className='flex gap-4'>
-                                <div className='w-16 h-16 rounded-2xl overflow-hidden relative'>
-                                    <Image src={producto.imagen[0]} alt={producto.nombre} fill />
-                                </div>
+                            <div key={id} className='flex items-center gap-4'>
+                                <Image
+                                    src={producto.imagen[0]}
+                                    alt={producto.nombre}
+                                    width={64}
+                                    height={64}
+                                    className='rounded-xl object-contain border border-_gray dark:border-_darkText'
+                                />
                                 <div>
-                                    <span>{producto.nombre}</span>
-                                    <span className='line-clamp-2'>{producto.descripcion}</span>
+                                    <span className='text-xl font-semibold text-_white'>{producto.nombre}</span>
+                                    <span className='line-clamp-2 text-xs font-medium text-_grayText'>{producto.descripcion}</span>
                                 </div>
                             </div>
                         ))}
                     </div>
-                    : <div>
-                        No hay historial
+                    : <div className='h-full flex justify-center items-center'>
+                        <p className='font-medium text-_grayText'>No hay QRs en este estado</p>
                     </div>}
             </section>
         </ModalPage>
