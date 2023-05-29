11c82a5562c8f1cfe483cf96b7a1e19742c9b449
omgismartinez <martnzomg@gmail.com>
2023-05-28 23:54:43 -0600

    🌌 refactor (home): main tabs independent logic

--- app/page.tsx
+++ app/page.tsx
@@ -7,48 +7,16 @@ import { getCurrentUser } from '@/hooks/auth'
 import Navbar from '@/components/navbar'
 import MainTabs from '@/components/main-tabs'
 import { Button } from '@/ui/button'
-import { headers } from 'next/headers'
-
-async function getQr(origin: string, email: string,) {
-  const res = await fetch(origin + '/api/qr/' + email, {
-    next: {
-      revalidate: 0,
-    },
-  })
-  if (!res.ok) {
-    throw new Error('Failed to fetch qr')
-  }
-  return res.json()
-}
-
-async function getCategorias(origin: string) {
-  const res = await fetch(origin + '/api/categorias', {
-    next: {
-      revalidate: 0,
-    },
-  })
-  if (!res.ok) {
-    throw new Error('Failed to fetch categorias')
-  }
-  return res.json()
-}
 
 export default async function Home() {
-  const headersList = headers()
-  const origin = headersList.get('x-origin') || ''
-
   const user = await getCurrentUser()
-  const qr = await getQr(origin, user?.email ?? '')
-  const categories = await getCategorias(origin)
   return (
     <main>
       <Navbar />
       <div className='px-4 min-h-screen relative max-w-7xl mx-auto'>
         <h1 className='text-5xl font-bold leading-loose truncate'>{user?.user_metadata.name}</h1>
-        <MainTabs
-          qr={qr}
-          categories={categories}
-        />
+        {/* @ts-expect-error Async Server Component */}
+        <MainTabs />
         <Link href={'/metadata'} className='rounded-2xl'>
           <div className='absolute z-30 w-16 right-4'>
             <Button type='submit' className='w-16 fixed bottom-8'>
