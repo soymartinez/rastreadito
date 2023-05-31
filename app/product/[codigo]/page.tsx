a56699a004e9c182c9ff8a05b5d8d44e9fcf8287
omgismartinez <martnzomg@gmail.com>
2023-05-31 13:52:48 -0600

    🌌 refactor (codigo): only navigate to the previous history entry.

--- app/product/[codigo]/page.tsx
+++ app/product/[codigo]/page.tsx
@@ -50,7 +50,7 @@ export default async function Product({ params }: { params: { codigo: string } }
   return (
     <main className='px-4 min-h-screen relative md:max-w-4xl xl:max-w-7xl mx-auto overflow-hidden'>
       <div className='flex justify-center items-center py-8 relative'>
-        <Back pushRoute='/' className='absolute left-0' />
+        <Back className='absolute left-0' />
         <h1 className='font-bold text-xl'>Producto</h1>
       </div>
       <div className='py-4'>
