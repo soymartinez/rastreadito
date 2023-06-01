4c4b885bc92699c603a256ca7684167363130984
omgismartinez <martnzomg@gmail.com>
2023-06-01 16:37:22 -0600

    🌌 refactor (categoria/nombre): products count

--- app/@modal/(.)categoria/[nombre]/page.tsx
+++ app/@modal/(.)categoria/[nombre]/page.tsx
@@ -43,11 +43,16 @@ async function Categorias({ qr }: { qr: Promise<QrProductType[]> }) {
     const data = await qr
     return (
         <div className='overflow-auto scrollbar-thin'>
+            {data.length > 0 &&
+                <div className='whitespace-nowrap px-3 pb-6'>
+                    <span className='font-bold'>{data.length}</span>{' '}
+                    {data.length > 1 ? 'productos registrados' : 'producto registrado'}
+                </div>}
             {data.length === 0
                 ? <Empty title='No hay productos en esta categoría.' description='Registra tu primer producto.' />
                 : data.map(({ producto, id, codigo, estatus }) => (
                     <Link href={`/product/${codigo}`}>
-                        <div key={id} className='flex justify-between gap-4 px-4 py-2 hover:bg-_gray dark:hover:bg-_dark/50'>
+                        <div key={id} className='flex justify-between gap-4 px-3 py-2 hover:bg-_gray dark:hover:bg-_dark/50'>
                             <h1 className='text-xl font-semibold text-_dark dark:text-_white dark:hover:text-opacity-70'>
                                 {producto.nombre} <span className='text-_grayText dark:text-_primary text-sm'>#{producto.categoria} {id}</span>
                             </h1>
@@ -66,11 +71,11 @@ async function Categorias({ qr }: { qr: Promise<QrProductType[]> }) {
 const FadingLoader = () => {
     return (
         <ContentLoader
-            className='w-full h-full px-4 py-2'
+            className='w-full h-full px-3'
             backgroundColor="#f3f3f3"
             foregroundColor="#ecebeb"
         >
-            <rect x="0" y="0" rx="5" ry="5" width="100%" height="32" />
+            <rect x="0" y="0" rx="5" ry="5" width="200" height="20" />
             <rect x="0" y="48" rx="5" ry="5" width="100%" height="32" />
             <rect x="0" y="96" rx="5" ry="5" width="100%" height="32" />
             <rect x="0" y="144" rx="5" ry="5" width="100%" height="32" />
