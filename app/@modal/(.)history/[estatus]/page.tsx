7aee5c3a8610f07add598b487987f6f2ed67ccf5
omgismartinez <martnzomg@gmail.com>
2023-06-01 18:51:55 -0600

    🌌 refactor (history/estatus): loader

--- app/@modal/(.)history/[estatus]/page.tsx
+++ app/@modal/(.)history/[estatus]/page.tsx
@@ -2,9 +2,12 @@ import Empty from '@/components/empty'
 import ModalPage from '@/components/modal/modal-page'
 import { useSupabaseServer } from '@/hooks/auth'
 import { prisma } from '@/lib/prisma'
+import { QrProductType } from '@/types'
 import { Estatus } from '@prisma/client'
 import { User } from '@supabase/supabase-js'
 import Image from 'next/image'
+import { Suspense } from 'react'
+import ContentLoader from 'react-content-loader'
 
 const getStatusHistory = async (estatus: Estatus, user: User | null) => {
     const history = await prisma.qr.findMany({
@@ -24,7 +27,7 @@ const getStatusHistory = async (estatus: Estatus, user: User | null) => {
 
 export default async function HistoryStatus({ params }: { params: { estatus: string } }) {
     const { user } = await useSupabaseServer()
-    const history = await getStatusHistory(params.estatus.toUpperCase() as Estatus, user)
+    const history = getStatusHistory(params.estatus.toUpperCase() as Estatus, user)
 
     const title = () => {
         const estatus = params.estatus.toUpperCase() as Estatus
@@ -38,27 +41,57 @@ export default async function HistoryStatus({ params }: { params: { estatus: str
             <div className='sticky top-0 z-50 bg-_white dark:bg-_dark border-b border-_gray dark:border-_darkText'>
                 <h1 className='text-5xl font-bold leading-loose truncate'>{title()}</h1>
             </div>
-            <section>
-                {history.length > 0
-                    ? <div className='flex flex-col gap-4'>
-                        {history.map(({ id, producto }) => (
-                            <div key={id} className='flex items-center gap-4'>
-                                <Image
-                                    src={producto.imagen[0]}
-                                    alt={producto.nombre}
-                                    width={64}
-                                    height={64}
-                                    className='rounded-xl object-contain border border-_gray dark:border-_darkText'
-                                />
-                                <div>
-                                    <span className='text-xl font-semibold'>{producto.nombre}</span>
-                                    <span className='line-clamp-2 text-xs font-medium text-_grayText'>{producto.descripcion}</span>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                    : <Empty title='Aún no hay productos en este estado.' description='Genera un nuevo QR.' />}
-            </section>
+            <Suspense fallback={<FadingLoader />}>
+                {/* @ts-expect-error Async Server Component */}
+                <History history={history} />
+            </Suspense>
         </ModalPage>
     )
 }
+
+async function History({ history }: { history: Promise<QrProductType[]> }) {
+    const data = await history
+    return (
+        <section>
+            {data.length > 0
+                ? <div className='flex flex-col gap-4'>
+                    {data.map(({ id, producto }) => (
+                        <div key={id} className='flex items-center gap-4'>
+                            <Image
+                                src={producto.imagen[0]}
+                                alt={producto.nombre}
+                                width={64}
+                                height={64}
+                                className='rounded-xl object-contain border border-_gray dark:border-_darkText'
+                            />
+                            <div>
+                                <span className='text-xl font-semibold'>{producto.nombre}</span>
+                                <span className='line-clamp-2 text-xs font-medium text-_grayText'>{producto.descripcion}</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                : <Empty title='Aún no hay productos en este estado.' description='Genera un nuevo QR.' />}
+        </section>
+    )
+}
+
+const FadingLoader = () => (
+    <ContentLoader
+        className='w-full h-96'
+        backgroundColor='#f3f3f3'
+        foregroundColor='#ecebeb'
+    >
+        <rect x='0' y='0' rx='5' ry='5' width='64' height='64' />
+        <rect x='70' y='8' rx='5' ry='5' width='200' height='16' />
+        <rect x='70' y='28' rx='5' ry='5' width='100%' height='26' />
+
+        <rect x='0' y='80' rx='5' ry='5' width='64' height='64' />
+        <rect x='70' y='88' rx='5' ry='5' width='180' height='16' />
+        <rect x='70' y='108' rx='5' ry='5' width='80%' height='26' />
+
+        <rect x='0' y='160' rx='5' ry='5' width='64' height='64' />
+        <rect x='70' y='168' rx='5' ry='5' width='210' height='16' />
+        <rect x='70' y='188' rx='5' ry='5' width='70%' height='26' />
+    </ContentLoader>
+)
