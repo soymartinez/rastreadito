b84269cbfd25cc9522131b410887400a15fd37ab
omgismartinez <martnzomg@gmail.com>
2023-06-02 23:02:19 -0600

    🚀 feat (actions): destroy product

---
+++ app/actions/destroy-product.ts
@@ -0,0 +1,21 @@
+'use server'
+
+import { prisma } from '@/lib/prisma'
+import { revalidatePath } from 'next/cache'
+
+export async function destroyProduct(ids: number[]) {
+    const res = await prisma.qr.updateMany({
+        data: {
+            estatus: 'DESTRUIDO',
+        },
+        where: {
+            id: {
+                in: ids
+            }
+        },
+    })
+
+    revalidatePath('/')
+    revalidatePath('/history')
+    return res
+}
