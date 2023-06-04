0f39c335e1b6f1b8b5152d340c489a6b28e76937
omgismartinez <martnzomg@gmail.com>
2023-06-03 22:24:14 -0600

    🚀 feat (product): edit modal page

---
+++ app/@modal/(.)product/[codigo]/edit/page.tsx
@@ -0,0 +1,42 @@
+import FormMetadata from '@/components/form-metadata'
+import ModalPage from '@/components/modal/modal-page'
+import { prisma } from '@/lib/prisma'
+import { Back } from '@/ui/back'
+
+async function getProduct(codigo: string) {
+  const res = await prisma.qr.findUnique({
+    where: {
+      codigo,
+    },
+    include: {
+      producto: true,
+    },
+  })
+
+  return res
+}
+
+async function getCategorias() {
+  const res = await prisma.categoria.findMany()
+  return res
+}
+
+export default async function EditProduct({ params }: { params: { codigo: string } }) {
+  const producto = await getProduct(params.codigo)
+  const categorias = await getCategorias()
+  return (
+    <ModalPage>
+      <div className='flex justify-center items-center py-8 relative'>
+        <Back className='absolute left-0' />
+        <h1 className='font-bold text-xl'>Editar producto</h1>
+      </div>
+      <div className='py-4'>
+        <h1 className='text-5xl font-bold'>{producto?.producto.nombre}</h1>
+      </div>
+      <FormMetadata
+        producto={producto?.producto}
+        categorias={categorias}
+      />
+    </ModalPage>
+  )
+}
