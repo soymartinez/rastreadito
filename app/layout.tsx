a2d27b37c6b62cfc2960d83fc46f1f620b39c79e
Martinez <72507996+omgismartinez@users.noreply.github.com>
2023-06-22 02:05:08 -0600

    🌌 refactor (layout): analytics disabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default async function RootLayout({
               {children}
               {modal}
             </>
-            <Analytics />
+            {/* <Analytics /> */}
           </SupabaseProvider>
         </ThemeProvider>
         <Toast />
