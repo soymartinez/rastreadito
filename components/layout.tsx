f7bc7afc9088b0d32ee475838d49cd22133bd455
omgismartinez <martnzomg@gmail.com>
2023-07-30 23:56:37 -0600

    🚀 feat: layout boundary

---
+++ components/layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+interface LayoutBoundaryProps {
+    children: React.ReactNode
+}
+
+export default function LayoutBoundary({ children }: LayoutBoundaryProps) {
+    return (
+        <main
+            className='
+                px-4
+                min-h-screen
+                relative
+                max-w-7xl
+                mx-auto
+            '
+        >
+            {children}
+        </main>
+    )
+}
