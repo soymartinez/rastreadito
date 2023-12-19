96f276e06d1abd1023abad0101ab86991a4f1860
omgismartinez <martnzomg@gmail.com>
2023-12-18 19:36:31 -0600

    refactor: home modals indent

--- app/(home)/@modal/(.)categoria/[nombre]/data.tsx
+++ app/(home)/@modal/(.)categoria/[nombre]/data.tsx
@@ -8,47 +8,47 @@ import Link from 'next/link'
 import { useState } from 'react'
 
 export default function CategoriasData({ data }: { data: QrProductType[] }) {
-    const [search, setSearch] = useState('')
-    const filteredData = data.filter(({ producto, id }) => {
-        return producto.nombre.toLowerCase().includes(search.toLowerCase()) || id.toString().includes(search)
-    })
-    return (
-        <section className='flex flex-col gap-6'>
-            {data.length > 0 ? (
-                <>
-                    <Input
-                        variant='search'
-                        name='search'
-                        onChange={(e) => setSearch(e.target.value)}
-                        placeholder='Buscar por nombre'
-                        className='p-1 border-2'
-                    />
-                    <div className='overflow-auto scrollbar-thin'>
-                        {filteredData.length > 0 ? (
-                            <>
-                                <div className='whitespace-nowrap px-3 pb-6'>
-                                    <span className='font-bold'>{filteredData.length}</span>{' '}
-                                    {filteredData.length > 1 ? 'productos registrados' : 'producto registrado'}
-                                </div>
-                                {filteredData.map(({ producto, id, codigo, estatus }) => (
-                                    <Link key={id} href={`/product/${codigo}`}>
-                                        <div className='flex justify-between gap-4 px-3 py-2 hover:bg-_gray/80 dark:bg-_dark dark:hover:bg-_darkText/50'>
-                                            <h1 className='text-xl font-semibold truncate'>
-                                                {producto.nombre} <span className='text-_grayText dark:text-_primary text-sm'>#{producto.categoria} {id}</span>
-                                            </h1>
-                                            <div>
-                                                {estatus === 'ACTIVO' && <ActiveButton />}
-                                                {estatus === 'USADO' && <UseButton />}
-                                                {estatus === 'DESTRUIDO' && <DestroyButton />}
-                                            </div>
-                                        </div>
-                                    </Link>
-                                ))}
-                            </>
-                        ) : <Empty title='No se encontraron productos con ese nombre.' />}
+  const [search, setSearch] = useState('')
+  const filteredData = data.filter(({ producto, id }) => {
+    return producto.nombre.toLowerCase().includes(search.toLowerCase()) || id.toString().includes(search)
+  })
+  return (
+    <section className='flex flex-col gap-6'>
+      {data.length > 0 ? (
+        <>
+          <Input
+            variant='search'
+            name='search'
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder='Buscar por nombre'
+            className='p-1 border-2'
+          />
+          <div className='overflow-auto scrollbar-thin'>
+            {filteredData.length > 0 ? (
+              <>
+                <div className='whitespace-nowrap px-3 pb-6'>
+                  <span className='font-bold'>{filteredData.length}</span>{' '}
+                  {filteredData.length > 1 ? 'productos registrados' : 'producto registrado'}
+                </div>
+                {filteredData.map(({ producto, id, codigo, estatus }) => (
+                  <Link key={id} href={`/product/${codigo}`}>
+                    <div className='flex justify-between gap-4 px-3 py-2 hover:bg-_gray/80 dark:bg-_dark dark:hover:bg-_darkText/50'>
+                      <h1 className='text-xl font-semibold truncate'>
+                        {producto.nombre} <span className='text-_grayText dark:text-_primary text-sm'>#{producto.categoria} {id}</span>
+                      </h1>
+                      <div>
+                        {estatus === 'ACTIVO' && <ActiveButton />}
+                        {estatus === 'USADO' && <UseButton />}
+                        {estatus === 'DESTRUIDO' && <DestroyButton />}
+                      </div>
                     </div>
-                </>
-            ) : <Empty title='No hay productos en esta categoría.' />}
-        </section>
-    )
+                  </Link>
+                ))}
+              </>
+            ) : <Empty title='No se encontraron productos con ese nombre.' />}
+          </div>
+        </>
+      ) : <Empty title='No hay productos en esta categoría.' />}
+    </section>
+  )
 }
--- app/(home)/@modal/(.)history/[estatus]/data.tsx
+++ app/(home)/@modal/(.)history/[estatus]/data.tsx
@@ -8,42 +8,42 @@ import Link from 'next/link'
 import React from 'react'
 
 export default function DataHistoryEstatus({ data }: { data: QrProductType[] }) {
-    const [history] = React.useState(data)
-    const [search, setSearch] = React.useState('')
-    return (
-        <section className='flex flex-col gap-6'>
-            {history.length > 0
-                ? <>
-                    <Input
-                        variant='search'
-                        name='search'
-                        onChange={(e) => setSearch(e.target.value)}
-                        placeholder='Buscar por nombre'
-                        className='p-1 h-full border-2'
+  const [history] = React.useState(data)
+  const [search, setSearch] = React.useState('')
+  return (
+    <section className='flex flex-col gap-6'>
+      {history.length > 0
+        ? <>
+          <Input
+            variant='search'
+            name='search'
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder='Buscar por nombre'
+            className='p-1 h-full border-2'
+          />
+          <div className='flex flex-col gap-4'>
+            {history
+              .filter(({ producto }) => producto.nombre.toLowerCase().includes(search.toLowerCase()))
+              .map(({ id, producto, codigo }) => (
+                <Link href={`/product/${codigo}`} key={id}>
+                  <div className='flex items-center gap-4'>
+                    <Image
+                      src={producto.imagen[0]}
+                      alt={producto.nombre}
+                      width={64}
+                      height={64}
+                      className='rounded-xl object-contain border border-_gray dark:border-_darkText'
                     />
-                    <div className='flex flex-col gap-4'>
-                        {history
-                            .filter(({ producto }) => producto.nombre.toLowerCase().includes(search.toLowerCase()))
-                            .map(({ id, producto, codigo }) => (
-                                <Link href={`/product/${codigo}`} key={id}>
-                                    <div className='flex items-center gap-4'>
-                                        <Image
-                                            src={producto.imagen[0]}
-                                            alt={producto.nombre}
-                                            width={64}
-                                            height={64}
-                                            className='rounded-xl object-contain border border-_gray dark:border-_darkText'
-                                        />
-                                        <div>
-                                            <span className='text-xl font-semibold'>{producto.nombre}</span>
-                                            <span className='line-clamp-2 text-xs font-medium text-_grayText'>{producto.descripcion}</span>
-                                        </div>
-                                    </div>
-                                </Link>
-                            ))}
+                    <div>
+                      <span className='text-xl font-semibold'>{producto.nombre}</span>
+                      <span className='line-clamp-2 text-xs font-medium text-_grayText'>{producto.descripcion}</span>
                     </div>
-                </>
-                : <Empty title='Aún no hay productos en este estado.' />}
-        </section>
-    )
+                  </div>
+                </Link>
+              ))}
+          </div>
+        </>
+        : <Empty title='Aún no hay productos en este estado.' />}
+    </section>
+  )
 }
