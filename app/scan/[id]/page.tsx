545213de9c4791ef6b05b9a2bb9228710bd3114f
omgismartinez <martnzomg@gmail.com>
2023-12-22 19:40:20 -0600

    refactor: scan routes indent

--- app/scan/[id]/page.tsx
+++ app/scan/[id]/page.tsx
@@ -13,29 +13,29 @@ interface Params {
 }
 
 async function getData({ origin, search }: Params): Promise<GetDataType> {
-    const res = await fetch(`${origin}/api/qr${search}`)
+  const res = await fetch(`${origin}/api/qr${search}`)
 
-    if (!res.ok) {
-        throw new Error('Error al obtener el QR')
-    }
+  if (!res.ok) {
+    throw new Error('Error al obtener el QR')
+  }
 
-    return res.json()
+  return res.json()
 }
 
 export default async function ScannedProduct() {
-    const header = headers()
-    const origin = header.get('x-origin') || ''
-    const search = header.get('x-search') || ''
+  const header = headers()
+  const origin = header.get('x-origin') || ''
+  const search = header.get('x-search') || ''
 
-    const qr = await getData({ origin, search })
-    return (
-        <main className='min-h-screen relative max-w-7xl mx-auto'>
-            <div className='px-4 py-8 absolute inset-x-0 top-0'>
-                <div className='relative h-7 flex items-center'>
-                    <Back className='absolute left-0' />
-                </div>
-            </div>
-            <Scanned {...qr} />
-        </main>
-    )
+  const qr = await getData({ origin, search })
+  return (
+    <main className='min-h-screen relative max-w-7xl mx-auto'>
+      <div className='px-4 py-8 absolute inset-x-0 top-0'>
+        <div className='relative h-7 flex items-center'>
+          <Back className='absolute left-0' />
+        </div>
+      </div>
+      <Scanned {...qr} />
+    </main>
+  )
 }
--- app/scan/layout.tsx
+++ app/scan/layout.tsx
@@ -1,14 +1,14 @@
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-    title: 'Escanear',
-    description: 'Escanea el código QR de un producto cannábico y accede a su metadata.',
+  title: 'Escanear',
+  description: 'Escanea el código QR de un producto cannábico y accede a su metadata.',
 }
 
 export default async function ScanLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <main>
-            {children}
-        </main>
-    )
+  return (
+    <main>
+      {children}
+    </main>
+  )
 }
