33ef3b74a75d926b87798172abc5f6a7693976f4
martinez <martnzomg@gmail.com>
2024-01-11 22:51:13 -0600

    refactor (auth): import path + tailwind format

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,27 +1,27 @@
 import { SignIn, SignInWhitGoogle, SignUp } from '@/components/auth'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/ui/tabs'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 export default async function Auth() {
   return (
-    <section className='flex items-center justify-center min-h-screen px-4 py-16'>
+    <section className='flex min-h-screen items-center justify-center px-4 py-16'>
       <Tabs defaultValue='login' className='w-full max-w-md'>
         <TabsContent value='login'>
-          <h1 className='text-3xl leading-normal font-semibold text-center'>Bienvenido</h1>
-          <h3 className='text-_grayText font-semibold text-center'>Por favor ingrese sus datos</h3>
+          <h1 className='text-center text-3xl font-semibold leading-normal'>Bienvenido</h1>
+          <h3 className='text-center font-semibold text-_grayText'>Por favor ingrese sus datos</h3>
         </TabsContent>
         <TabsContent value='signup'>
-          <h1 className='text-3xl leading-normal font-semibold text-center'>Crear una cuenta</h1>
-          <h3 className='text-_grayText font-semibold text-center'>Por favor ingrese sus datos</h3>
+          <h1 className='text-center text-3xl font-semibold leading-normal'>Crear una cuenta</h1>
+          <h3 className='text-center font-semibold text-_grayText'>Por favor ingrese sus datos</h3>
         </TabsContent>
-        <TabsList className='inline-flex w-full mt-7 bg-_primary p-1'>
+        <TabsList className='mt-7 inline-flex w-full bg-_primary p-1'>
           <TabsTrigger
             value='login'
-            className='w-full data-[state=active]:bg-_white dark:data-[state=active]:bg-_dark font-semibold data-[state=active]:text-_dark dark:data-[state=active]:text-_white text-_dark/50 dark:text-_dark/70'>
+            className='w-full font-semibold text-_dark/50 data-[state=active]:bg-_white data-[state=active]:text-_dark dark:text-_dark/70 dark:data-[state=active]:bg-_dark dark:data-[state=active]:text-_white'>
                         Iniciar sesión
           </TabsTrigger>
           <TabsTrigger
             value='signup'
-            className='w-full data-[state=active]:bg-_white dark:data-[state=active]:bg-_dark font-semibold data-[state=active]:text-_dark dark:data-[state=active]:text-_white text-_dark/50 dark:text-_dark/70'>
+            className='w-full font-semibold text-_dark/50 data-[state=active]:bg-_white data-[state=active]:text-_dark dark:text-_dark/70 dark:data-[state=active]:bg-_dark dark:data-[state=active]:text-_white'>
                         Registrarse
           </TabsTrigger>
         </TabsList>
@@ -32,12 +32,12 @@ export default async function Auth() {
           <SignUp />
         </TabsContent>
         <section>
-          <div className='flex items-center gap-6 my-8'>
-            <div className='bg-_primary w-full h-px rounded-full' />
-            <span className='text-_grayText font-medium whitespace-nowrap'>O Continúa con</span>
-            <div className='bg-_primary w-full h-px rounded-full' />
+          <div className='my-8 flex items-center gap-6'>
+            <div className='h-px w-full rounded-full bg-_primary' />
+            <span className='whitespace-nowrap font-medium text-_grayText'>O Continúa con</span>
+            <div className='h-px w-full rounded-full bg-_primary' />
           </div>
-          <div className='flex justify-center items-center gap-4'>
+          <div className='flex items-center justify-center gap-4'>
             <SignInWhitGoogle />
           </div>
         </section>
