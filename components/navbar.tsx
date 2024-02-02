1fc349b3400ab464c01eadfc907b71cdcc451cb1
martinez <martnzomg@gmail.com>
2024-02-01 18:15:25 -0600

    🧩 refactor: navbar improved

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,91 +1,88 @@
+'use client'
+
 import Link from 'next/link'
-import { Fingerprint, Scan } from 'lucide-react'
 import { Button, buttonVariants } from '@/components/ui/button'
-import { User } from '@supabase/supabase-js'
 import clsx from 'clsx'
+import { AnimatePresence } from 'framer-motion'
+import NavbarMenu from './navbar-menu'
+import { useEffect, useState } from 'react'
+import { Menu } from 'lucide-react'
+import { siteConfig } from '@/config/site'
+import { usePathname } from 'next/navigation'
 
-interface NavbarProps {
-  user?: User | null
-}
+export default function Navbar() {
+  const pathname = usePathname()
+  const [isActive, setIsActive] = useState(false)
 
-export default function Navbar({ user }: NavbarProps) {
+  useEffect(() => {
+    if (isActive) setIsActive(false)
+  }, [pathname])
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent): void => {
+      if (event.keyCode === 27) {
+        setIsActive(false)
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
   return (
     <nav className='sticky top-0 z-50 bg-white/60 backdrop-blur-[8px] dark:bg-_dark/60'>
-      <div className='relative mx-auto flex h-16 max-w-7xl items-center justify-between px-4'>
-        <Link href={'/'} className='z-10 text-2xl font-black uppercase italic'>
+      <div className='relative mx-auto flex h-16 max-w-full items-center justify-between px-4'>
+        <Link href={'/'} className='z-10 -mx-0.5 p-0.5 text-2xl font-black uppercase italic'>
           rastreadito
         </Link>
-        <ul className='absolute inset-x-0 mx-auto flex w-auto items-center justify-center text-center text-sm font-medium'>
-          <li className='flex h-[30px] items-center justify-center'>
-            <Link href={'/'} className='whitespace-nowrap rounded-full px-3 py-[5px] hover:bg-white/10'>
-              Inicio
-            </Link>
-          </li>
-          <li className='flex h-[30px] items-center justify-center'>
-            <Link href={'/explore'} className='whitespace-nowrap rounded-full px-3 py-[5px] hover:bg-white/10'>
-              Explorar
-            </Link>
-          </li>
-          <li className='flex h-[30px] items-center justify-center'>
-            <Link href={'/how-it-works'} className='whitespace-nowrap rounded-full px-3 py-[5px] hover:bg-white/10'>
-              ¿Cómo funciona?
-            </Link>
-          </li>
-          <li className='flex h-[30px] items-center justify-center'>
-            <Link href={'/pricing'} className='whitespace-nowrap rounded-full px-3 py-[5px] hover:bg-white/10'>
-              Precios
-            </Link>
-          </li>
-        </ul>
-        <div className='flex items-center gap-2'>
-          {/* <Link href={'/scan'} className='rounded-full'>
-            <Button
-              variant={'ghost'}
-              size={'icon'}
-              className='border-2 border-_gray dark:border-_darkText'
-            >
-              <Scan className='dark:text-_white' />
-            </Button>
-          </Link> */}
-          {/* <Link href={user ? '/account' : '/auth'} className='rounded-full'>
-            {user
-              ? <div className='w-4 h-4 border-2 border-_gray dark:border-_darkText bg-_primary rounded-full' />
-              : <Button
-                size='icon'
-                className='text-[14px] border-2 dark:border-_darkText whitespace-nowrap'
+        <ul className='absolute inset-x-0 mx-auto hidden w-auto items-center justify-center text-center text-sm font-medium mid:flex'>
+          {siteConfig.mainNav[0].items.map((nav) => (
+            <li key={nav.title} className='flex h-[30px] items-center justify-center'>
+              <Link
+                href={nav.href}
+                className='whitespace-nowrap rounded-full px-3 py-[5px] transition-colors hover:bg-_darkText hover:text-_white'
               >
-                <Fingerprint />
-              </Button>
-            }
-          </Link> */}
+                {nav.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className='z-50 hidden items-center gap-2 mid:flex'>
           <Link
-            href='/auth/login'
+            href='/login'
             className={clsx(buttonVariants({
               size: 'sm',
-              className: 'text-sm px-4 h-[30px] font-medium bg-transparent text-white hover:bg-white/5'
+              className: '!text-sm px-4 h-[30px] !font-medium bg-transparent hover:bg-black/5'
             }))}
-            style={{
-              fontSize: 14,
-              fontWeight: 500
-            }}
           >
-            Iniciar sesion
+            Iniciar sesión
           </Link>
           <Link
-            href='/auth/register'
+            href='/register'
             className={clsx(buttonVariants({
               size: 'sm',
-              className: 'text-sm px-4 h-[30px] font-medium',
+              className: 'px-4 h-[30px] !font-medium !bg-_darkText !text-_white',
             }))}
-            style={{
-              fontSize: 14,
-              fontWeight: 500
-            }}
           >
             Registrate
           </Link>
         </div>
+
+        <Button
+          onClick={() => setIsActive(!isActive)}
+          className='h-min rounded-sm bg-transparent !p-1 hover:bg-_darkText/5 mid:hidden'
+        >
+          <Menu className='size-7' />
+        </Button>
       </div>
+
+      {/* MENU */}
+      <AnimatePresence mode='wait'>
+        {isActive && <NavbarMenu setIsActive={setIsActive} />}
+      </AnimatePresence>
     </nav >
   )
 }
