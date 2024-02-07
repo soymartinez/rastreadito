a9fdc9b19c4774666d893498bd9f6f9b09cb3b26
martinez <martnzomg@gmail.com>
2024-02-06 18:18:59 -0600

    🧩 refactor (middleware): format

--- middleware.ts
+++ middleware.ts
@@ -4,42 +4,42 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-    const requestHeaders = new Headers(req.headers)
-    requestHeaders.set('x-search', req.nextUrl.search)
-    requestHeaders.set('x-origin', req.nextUrl.origin)
+  const requestHeaders = new Headers(req.headers)
+  requestHeaders.set('x-search', req.nextUrl.search)
+  requestHeaders.set('x-origin', req.nextUrl.origin)
 
-    const res = NextResponse.next({
-        request: {
-            headers: requestHeaders,
-        },
-    })
+  const res = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  })
 
-    const supabase = createMiddlewareSupabaseClient({ req, res })
-    const {
-        data: { session },
-    } = await supabase.auth.getSession()
+  const supabase = createMiddlewareSupabaseClient({ req, res })
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
 
-    const url = new URL(req.url)
+  const url = new URL(req.url)
 
-    if (!session && !url.pathname.startsWith('/auth')) {
-        url.pathname = '/auth'
-        return NextResponse.redirect(url)
-    }
+  if (!session && !url.pathname.startsWith('/auth')) {
+    url.pathname = '/auth'
+    return NextResponse.redirect(url)
+  }
 
-    if (session && url.pathname.startsWith('/auth')) {
-        url.pathname = '/'
-        return NextResponse.redirect(url)
-    }
+  if (session && url.pathname.startsWith('/auth')) {
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
 
-    return res
+  return res
 }
 
 export const config = {
-    matcher: [
-        '/auth',
-        '/account',
-        '/metadata',
-        '/metadata/generate/:path*',
-        '/history',
-    ],
+  matcher: [
+    '/auth',
+    '/account',
+    '/metadata',
+    '/metadata/generate/:path*',
+    '/history',
+  ],
 }
