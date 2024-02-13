ab1fa3e397e4547199082819ab997fcc81175403
martinez <martnzomg@gmail.com>
2024-02-12 18:22:26 -0600

    🧩 refactor (layout): root improved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
 import './globals.css'
 import { Metadata } from 'next'
-import { Analytics } from '@vercel/analytics/react'
+// import { Analytics } from '@vercel/analytics/react'
 import { font } from '@/lib/font'
 import { ThemeProvider } from '@/components/provider'
 import SupabaseProvider from '@/components/supabase-provider'
 import Toast from '@/components/toast'
+import { siteConfig } from '@/config/site'
 
 export const metadata: Metadata = {
   title: {
-    default: 'Rastreadito',
-    template: '%s | Rastreadito',
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
   },
-  description: 'Rastreadito te permite seguir el rastro de tus productos cannábicos desde su origen hasta tu mano. Descubre su metadata y conoce toda su historia con esta app.',
+  description: siteConfig.description,
 }
 
 export default async function RootLayout({
@@ -21,10 +22,19 @@ export default async function RootLayout({
 }) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={`
-            scrollbar-thin scrollbar-thumb-_gray dark:scrollbar-thumb-_darkText scrollbar-thumb-rounded-full
-            bg-_white text-_dark dark:bg-_dark dark:text-_white ${font.className}
-          `}>
+      <body
+        className={`
+          bg-_white
+          text-_darkText
+          scrollbar-thin
+          scrollbar-thumb-_gray
+          scrollbar-thumb-rounded-full
+          dark:bg-_dark
+          dark:text-_white
+          dark:scrollbar-thumb-_darkText 
+          ${font.className}
+        `}
+      >
         <ThemeProvider>
           <SupabaseProvider>
             {children}
