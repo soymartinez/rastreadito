35c6b94c25c2e621f1c4a2883eed2f87d4db9ca7
martinez <martnzomg@gmail.com>
2024-05-28 22:52:57 -0600

    🧩 refactor (generate-qr): spread props

--- components/generate-qr.tsx
+++ components/generate-qr.tsx
@@ -5,13 +5,13 @@ import { QRCode, IProps } from 'react-qrcode-logo'
 export default function GenerateQr(props: IProps) {
   return (
     <QRCode
-      {...props}
       size={200}
       quietZone={20}
       bgColor={'#00e99e'}
       fgColor={'#1b1b1b'}
       qrStyle={'squares'}
       ecLevel={'L'}
+      {...props}
     />
   )
 }
