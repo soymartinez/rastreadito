6192da4265cfcd9adf8ea7e22a230f584b1ecd08
martinez <martnzomg@gmail.com>
2024-06-14 21:47:06 -0600

    ✨ feat: skeleton keyframe tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -116,10 +116,11 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        shimmer: {
+          '100%': {
+            transform: 'translateX(100%)',
+          },
+        },
       },
     },
   },
