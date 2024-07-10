8871e35b28344e5be000b0b3fe3a882d713c7c56
martinez <martnzomg@gmail.com>
2024-07-09 22:42:47 -0600

    🧩 refactor (toast): styles

--- components/toast.tsx
+++ components/toast.tsx
@@ -7,18 +7,22 @@ export default function Toast() {
   const { resolvedTheme } = useTheme()
   return (
     <Toaster
-      toastOptions={{
-        style: {
-          background: '#00e99e',
-          borderRadius: '1rem',
-          border: 'none',
-          fontWeight: 600,
-        },
-      }}
       position='bottom-right'
       theme={resolvedTheme === 'dark' ? 'dark' : 'light'}
       expand={true}
       closeButton
+      toastOptions={{
+        unstyled: true,
+        classNames: {
+          toast: 'group text-[13px] bg-primary gap-[6px] p-[16px] rounded-[8px] shadow-lg flex items-center w-full',
+          success: 'bg-primary text-dark',
+          error: 'bg-red text-white',
+          info: 'bg-blue text-white',
+          warning: 'bg-gray',
+          loader: 'bg-gray text-white',
+          closeButton: 'group-hover:opacity-100 transition opacity-0 bg-white text-dark',
+        }
+      }}
     />
   )
 }
